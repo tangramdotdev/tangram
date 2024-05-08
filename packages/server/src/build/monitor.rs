96e13975099b4ee8e83ebabdd4ceebc7c25baf90
m-hilgendorf <mike@hilgendorf.audio>
2024-05-08 13:44:15 -0500

    fix(server): use separate tasks for build enqueue/heartbeat and add LIMIT to build reaper

--- packages/server/src/build/monitor.rs
+++ packages/server/src/build/monitor.rs
@@ -3,7 +3,7 @@ use db::{Database, Query};
 use futures::{
 	future::{self, Either},
 	stream::FuturesUnordered,
-	TryStreamExt,
+	StreamExt,
 };
 use std::pin::pin;
 use tangram_client as tg;
@@ -12,7 +12,8 @@ use time::format_description::well_known::Rfc3339;
 
 pub struct Monitor {
 	stop: tokio::sync::watch::Sender<bool>,
-	task: std::sync::Mutex<Option<tokio::task::JoinHandle<()>>>,
+	heartbeat_task: std::sync::Mutex<Option<tokio::task::JoinHandle<()>>>,
+	enqueue_task: std::sync::Mutex<Option<tokio::task::JoinHandle<()>>>,
 }
 
 impl Server {
@@ -20,48 +21,70 @@ impl Server {
 		&self,
 		options: &options::BuildMonitor,
 	) -> tg::Result<Monitor> {
+		// Create the stop signal.
 		let (stop, _) = tokio::sync::watch::channel(false);
-		let task = tokio::task::spawn({
+
+		// Create the heartbeat task.
+		let heartbeat_task = tokio::task::spawn({
 			let server = self.clone();
 			let mut stop = stop.subscribe();
 			let interval = options.interval;
-			let heartbeat_timeout = options.heartbeat_timeout;
-			let dequeue_timeout = options.dequeue_timeout;
+			let timeout = options.heartbeat_timeout;
 			async move {
 				let mut interval = tokio::time::interval(interval);
 				loop {
 					let stop = stop.wait_for(|s| *s);
 					let tick = interval.tick();
+
+					// Wait for either the stop signal or an interval.
 					if let Either::Left(_) = future::select(pin!(stop), pin!(tick)).await {
 						break;
 					}
 
 					// Reap stale builds if the timeout is set.
-					let reap = async {
-						server
-							.try_reap_builds(heartbeat_timeout)
-							.await
-							.inspect_err(|error| tracing::error!(%error, "failed to reap builds"))
-							.ok();
-					};
-
-					// Reenqueue builds that have been erroneously dequeued but not started.
-					let reenqueue = async {
-						server
-							.try_reenqueue_builds(dequeue_timeout)
-							.await
-							.inspect_err(
-								|error| tracing::error!(%error, "failed to reenqueue builds"),
-							)
-							.ok();
-					};
-					futures::join!(reap, reenqueue);
+					server
+						.try_reap_builds(timeout)
+						.await
+						.inspect_err(|error| tracing::error!(%error, "failed to reap builds"))
+						.ok();
 				}
 			}
 		});
 
-		let task = std::sync::Mutex::new(Some(task));
-		Ok(Monitor { stop, task })
+		// Create the enqueue task.
+		let enqueue_task = tokio::task::spawn({
+			let server = self.clone();
+			let mut stop = stop.subscribe();
+			let interval = options.interval;
+			let timeout = options.dequeue_timeout;
+			async move {
+				let mut interval = tokio::time::interval(interval);
+				loop {
+					let stop = stop.wait_for(|s| *s);
+					let tick = interval.tick();
+
+					// Wait for either the stop signal or an interval.
+					if let Either::Left(_) = future::select(pin!(stop), pin!(tick)).await {
+						break;
+					}
+
+					// Reenqueue builds that were dequeued but not star
+					server
+						.try_reenqueue_builds(timeout)
+						.await
+						.inspect_err(|error| tracing::error!(%error, "failed to reap builds"))
+						.ok();
+				}
+			}
+		});
+
+		let heartbeat_task = std::sync::Mutex::new(Some(heartbeat_task));
+		let enqueue_task = std::sync::Mutex::new(Some(enqueue_task));
+		Ok(Monitor {
+			stop,
+			heartbeat_task,
+			enqueue_task,
+		})
 	}
 
 	async fn try_reap_builds(&self, timeout: std::time::Duration) -> tg::Result<()> {
@@ -73,40 +96,49 @@ impl Server {
 			.map_err(|source| tg::error!(!source, "failed to get a database connection"))?;
 
 		// Get all builds with heartbeat_at older than now - timeout.
-		#[derive(serde::Deserialize)]
-		struct Row {
-			id: tg::build::Id,
-		}
-		let p = connection.p();
-		let statement = format!(
-			"
-				select id from builds
-				where
-					heartbeat_at <= {p}1 and
-					status = 'started';
-			"
-		);
-		let time = time::OffsetDateTime::now_utc() - timeout;
-		let params = db::params!(time.format(&Rfc3339).unwrap());
-		let builds = connection
-			.query_all_into::<Row>(statement, params)
-			.await
-			.inspect_err(|error| tracing::error!(%error, "failed to perform query"))
-			.map_err(|source| tg::error!(!source, "failed to perform the query"))?
-			.into_iter()
-			.map(|row| row.id);
+		loop {
+			#[derive(serde::Deserialize)]
+			struct Row {
+				id: tg::build::Id,
+			}
+			let p = connection.p();
+			let statement = format!(
+				"
+					select id from builds
+					where
+						heartbeat_at <= {p}1 and
+						status = 'started'
+					limit 100;
+				"
+			);
+			let time = time::OffsetDateTime::now_utc() - timeout;
+			let params = db::params!(time.format(&Rfc3339).unwrap());
+			let builds = connection
+				.query_all_into::<Row>(statement, params)
+				.await
+				.inspect_err(|error| tracing::error!(%error, "failed to perform query"))
+				.map_err(|source| tg::error!(!source, "failed to perform the query"))?;
 
-		// Cancel builds that have missed a heartbeat.
-		builds
-			.into_iter()
-			.map(|id| async move {
-				let build = tg::Build::with_id(id);
-				build.cancel(self).await
-			})
-			.collect::<FuturesUnordered<_>>()
-			.try_collect::<Vec<_>>()
-			.await
-			.map_err(|source| tg::error!(!source, "failed to cancel builds"))?;
+			if builds.is_empty() {
+				break;
+			}
+
+			// Cancel builds that have missed a heartbeat.
+			builds
+				.into_iter()
+				.map(|row| async move {
+					let id = row.id;
+					let build = tg::Build::with_id(id.clone());
+					build
+						.cancel(self)
+						.await
+						.inspect_err(|error| tracing::error!(%error, %id, "failed to cancel build"))
+						.ok()
+				})
+				.collect::<FuturesUnordered<_>>()
+				.collect::<Vec<_>>()
+				.await;
+		}
 
 		Ok(())
 	}
@@ -146,9 +178,13 @@ impl Monitor {
 	}
 
 	pub async fn wait(&self) {
-		let task = self.task.lock().unwrap().take();
-		if let Some(task) = task {
-			task.await.ok();
+		let heartbeat_task = self.heartbeat_task.lock().unwrap().take();
+		if let Some(heartbeat_task) = heartbeat_task {
+			heartbeat_task.await.ok();
+		}
+		let enqueue_task = self.enqueue_task.lock().unwrap().take();
+		if let Some(enqueue_task) = enqueue_task {
+			enqueue_task.await.ok();
 		}
 	}
 }
