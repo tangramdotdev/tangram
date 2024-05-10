5ac94b1d8aa9692a67e7532a759cc9331a3896d1
m-hilgendorf <mike@hilgendorf.audio>
2024-05-10 12:07:43 -0500

    fix(server): dequeue builds from remotes

--- packages/server/src/build/queue.rs
+++ packages/server/src/build/queue.rs
@@ -1,5 +1,6 @@
 use crate::{BuildPermit, Server};
 use either::Either;
+use futures::FutureExt;
 use tangram_client as tg;
 
 impl Server {
@@ -9,19 +10,34 @@ impl Server {
 			let permit = self.build_semaphore.clone().acquire_owned().await.unwrap();
 			let permit = BuildPermit(Either::Left(permit));
 
-			// Dequeue a build.
-			let output = match self
-				.try_dequeue_build(tg::build::dequeue::Arg::default())
-				.await
-			{
-				Ok(Some(output)) => output,
-				Ok(None) => continue,
-				Err(error) => {
+			// Create an iterator over all remotes with build: true and the local queue.
+			let queues = self
+				.options
+				.remotes
+				.iter()
+				.filter(|remote| remote.build)
+				.map(|remote| {
+					remote
+						.client
+						.try_dequeue_build(tg::build::dequeue::Arg::default())
+						.boxed()
+				})
+				.chain(std::iter::once(
+					self.try_dequeue_build(tg::build::dequeue::Arg::default())
+						.boxed(),
+				));
+
+			// Try and dequeue a build locally or from one of the remotes.
+			let output = match futures::future::select_all(queues).await {
+				(Ok(Some(output)), _, _) => output,
+				(Ok(None), _, _) => continue,
+				(Err(error), _, _) => {
 					tracing::error!(?error, "failed to dequeue a build");
 					tokio::time::sleep(std::time::Duration::from_secs(1)).await;
 					continue;
 				},
 			};
+
 			let build = tg::Build::with_id(output.build);
 
 			// Start the build.
