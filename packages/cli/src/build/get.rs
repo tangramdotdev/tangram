8b90f754e435b9f2ae2e3b6e799c2683e0b47f40
David Yamnitsky <david@yamnitsky.com>
2024-05-17 22:23:46 -0400

    feat: pretty print get output if stdout is a tty

--- packages/cli/src/build/get.rs
+++ packages/cli/src/build/get.rs
@@ -1,4 +1,5 @@
 use crate::Cli;
+use crossterm::tty::IsTty;
 use tangram_client as tg;
 use tg::Handle as _;
 use tokio::io::AsyncWriteExt as _;
@@ -13,12 +14,26 @@ pub struct Args {
 impl Cli {
 	pub async fn command_build_get(&self, args: Args) -> tg::Result<()> {
 		let output = self.handle.get_build(&args.build).await?;
-		let json = serde_json::to_string(&output)
-			.map_err(|source| tg::error!(!source, "failed to serialize the output"))?;
-		tokio::io::stdout()
-			.write_all(json.as_bytes())
-			.await
-			.map_err(|source| tg::error!(!source, "failed to write the data"))?;
+		let mut stdout = tokio::io::stdout();
+		if stdout.is_tty() {
+			let output = serde_json::to_string_pretty(&output)
+				.map_err(|source| tg::error!(!source, "failed to serialize the output"))?;
+			stdout
+				.write_all(output.as_bytes())
+				.await
+				.map_err(|source| tg::error!(!source, "failed to write the data"))?;
+			stdout
+				.write_all(b"\n")
+				.await
+				.map_err(|source| tg::error!(!source, "failed to write"))?;
+		} else {
+			let output = serde_json::to_string(&output)
+				.map_err(|source| tg::error!(!source, "failed to serialize the output"))?;
+			stdout
+				.write_all(output.as_bytes())
+				.await
+				.map_err(|source| tg::error!(!source, "failed to write the data"))?;
+		};
 		Ok(())
 	}
 }
--- packages/cli/src/object/get.rs
+++ packages/cli/src/object/get.rs
@@ -1,4 +1,5 @@
 use crate::Cli;
+use crossterm::tty::IsTty;
 use tangram_client as tg;
 use tg::Handle as _;
 use tokio::io::AsyncWriteExt as _;
@@ -13,10 +14,26 @@ pub struct Args {
 impl Cli {
 	pub async fn command_object_get(&self, args: Args) -> tg::Result<()> {
 		let tg::object::get::Output { bytes, .. } = self.handle.get_object(&args.object).await?;
-		tokio::io::stdout()
-			.write_all(&bytes)
-			.await
-			.map_err(|source| tg::error!(!source, "failed to write the data"))?;
+		let mut stdout = tokio::io::stdout();
+		if stdout.is_tty() && !matches!(args.object, tg::object::Id::Leaf(_)) {
+			let json = serde_json::from_slice::<serde_json::Value>(&bytes)
+				.map_err(|source| tg::error!(!source, "failed to deserialize the object"))?;
+			let bytes = serde_json::to_vec_pretty(&json)
+				.map_err(|source| tg::error!(!source, "failed to serialize the object"))?;
+			stdout
+				.write_all(&bytes)
+				.await
+				.map_err(|source| tg::error!(!source, "failed to write the data"))?;
+			stdout
+				.write_all(b"\n")
+				.await
+				.map_err(|source| tg::error!(!source, "failed to write"))?;
+		} else {
+			stdout
+				.write_all(&bytes)
+				.await
+				.map_err(|source| tg::error!(!source, "failed to write the data"))?;
+		}
 		Ok(())
 	}
 }
