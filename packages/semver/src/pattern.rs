d4a5a65edb216db23a91e39241c15f102fa9a20a
David Yamnitsky <david@yamnitsky.com>
2024-07-08 10:01:28 -0400

    update semver

--- packages/semver/src/pattern.rs
+++ packages/semver/src/pattern.rs
@@ -1,8 +1,13 @@
 use crate::Version;
+use winnow::{
+	ascii::dec_uint,
+	combinator::{alt, opt, preceded, separated},
+	prelude::*,
+};
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Pattern {
-	pub comparators: Vec<Component>,
+	pub components: Vec<Component>,
 }
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -16,19 +21,24 @@ pub struct Component {
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[non_exhaustive]
 pub enum Operator {
-	Equal,
+	Caret,
+	Eq,
 	Greater,
 	GreaterEq,
 	Less,
 	LessEq,
 	Tilde,
-	Caret,
 }
+
+#[derive(Clone, Debug, derive_more::Display, derive_more::Error)]
+pub struct ParseError;
 
 impl Pattern {
 	#[must_use]
 	pub fn matches(&self, version: &Version) -> bool {
-		todo!()
+		self.components
+			.iter()
+			.all(|component| component.matches(version))
 	}
 }
 
@@ -41,28 +51,136 @@ impl Component {
 
 impl std::fmt::Display for Pattern {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		todo!()
+		for (i, component) in self.components.iter().enumerate() {
+			write!(f, "{component}")?;
+			if i != self.components.len() - 1 {
+				write!(f, ",")?;
+			}
+		}
+		Ok(())
 	}
 }
 
 impl std::str::FromStr for Pattern {
-	type Err = ();
+	type Err = ParseError;
 
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		todo!()
+		pattern.parse(s).ok().ok_or(ParseError)
 	}
 }
 
 impl std::fmt::Display for Component {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		todo!()
+		write!(f, "{}", self.operator)?;
+		write!(f, "{}", self.major)?;
+		if let Some(minor) = self.minor {
+			write!(f, ".{minor}")?;
+		}
+		if let Some(patch) = self.patch {
+			write!(f, ".{patch}")?;
+		}
+		Ok(())
 	}
 }
 
 impl std::str::FromStr for Component {
-	type Err = ();
+	type Err = ParseError;
 
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		todo!()
+		component.parse(s).ok().ok_or(ParseError)
+	}
+}
+
+impl std::fmt::Display for Operator {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Operator::Caret => write!(f, "^"),
+			Operator::Eq => write!(f, "="),
+			Operator::Greater => write!(f, ">"),
+			Operator::GreaterEq => write!(f, ">="),
+			Operator::Less => write!(f, "<"),
+			Operator::LessEq => write!(f, "<="),
+			Operator::Tilde => write!(f, "~"),
+		}
+	}
+}
+
+impl std::str::FromStr for Operator {
+	type Err = ParseError;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		operator.parse(s).ok().ok_or(ParseError)
+	}
+}
+
+fn pattern(input: &mut &str) -> PResult<Pattern> {
+	let components = separated(1.., component, ",").parse_next(input)?;
+	Ok(Pattern { components })
+}
+
+fn component(input: &mut &str) -> PResult<Component> {
+	let (operator, major, minor, patch) = (
+		operator,
+		dec_uint,
+		opt(preceded(".", dec_uint)),
+		opt(preceded(".", dec_uint)),
+	)
+		.parse_next(input)?;
+	Ok(Component {
+		operator,
+		major,
+		minor,
+		patch,
+	})
+}
+
+fn operator(input: &mut &str) -> PResult<Operator> {
+	alt((
+		"^".map(|_| Operator::Caret),
+		"=".map(|_| Operator::Eq),
+		">=".map(|_| Operator::GreaterEq),
+		">".map(|_| Operator::Greater),
+		"<=".map(|_| Operator::LessEq),
+		"<".map(|_| Operator::Less),
+		"~".map(|_| Operator::Tilde),
+	))
+	.parse_next(input)
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn parse() {
+		let left = "=1.2.3";
+		let right = Pattern {
+			components: vec![Component {
+				operator: Operator::Eq,
+				major: 1,
+				minor: Some(2),
+				patch: Some(3),
+			}],
+		};
+		assert_eq!(left.parse::<Pattern>().unwrap(), right);
+
+		let left = ">=1.2.3,<1.5";
+		let right = Pattern {
+			components: vec![
+				Component {
+					operator: Operator::GreaterEq,
+					major: 1,
+					minor: Some(2),
+					patch: Some(3),
+				},
+				Component {
+					operator: Operator::Less,
+					major: 1,
+					minor: Some(5),
+					patch: None,
+				},
+			],
+		};
+		assert_eq!(left.parse::<Pattern>().unwrap(), right);
 	}
 }
