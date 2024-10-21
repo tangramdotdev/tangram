5e3cbf8f5fe7820b357d0caf913b34f071eb25cb
Mike Hilgendorf <mike@hilgendorf.audio>
2024-10-21 11:41:18 -0500

    wip

--- packages/client/src/artifact/checkout.rs
+++ packages/client/src/artifact/checkout.rs
@@ -9,11 +9,8 @@ use tangram_http::{incoming::response::Ext as _, outgoing::request::Ext as _};
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
 pub struct Arg {
-	#[serde(default, skip_serializing_if = "is_false")]
-	pub bundle: bool,
-
 	#[serde(default = "return_true", skip_serializing_if = "is_true")]
-	pub dependencies: bool,
+	pub references: bool,
 
 	#[serde(default, skip_serializing_if = "is_false")]
 	pub force: bool,
@@ -85,10 +82,9 @@ impl tg::Client {
 impl Default for Arg {
 	fn default() -> Self {
 		Self {
-			bundle: false,
 			force: false,
 			path: None,
-			dependencies: true,
+			references: true,
 		}
 	}
 }
--- packages/server/src/artifact/checkout.rs
+++ packages/server/src/artifact/checkout.rs
@@ -2,8 +2,7 @@ use crate::{tmp::Tmp, Server};
 use dashmap::DashMap;
 use futures::{stream::FuturesUnordered, Stream, StreamExt as _, TryStreamExt as _};
 use std::{
-	collections::BTreeSet,
-	ffi::OsStr,
+	collections::{BTreeMap, BTreeSet},
 	os::unix::fs::PermissionsExt as _,
 	path::{Path, PathBuf},
 	pin::pin,
@@ -12,11 +11,12 @@ use std::{
 use tangram_client as tg;
 use tangram_futures::{stream::TryStreamExt as _, task::Task};
 use tangram_http::{incoming::request::Ext as _, Incoming, Outgoing};
+use tg::path::Ext as _;
 
 struct InnerArg<'a> {
 	arg: &'a tg::artifact::checkout::Arg,
 	artifact: &'a tg::Artifact,
-	depth: usize,
+	checkouts_path: &'a PathBuf,
 	existing_artifact: Option<&'a tg::Artifact>,
 	files: Arc<DashMap<tg::file::Id, PathBuf, fnv::FnvBuildHasher>>,
 	path: &'a PathBuf,
@@ -61,9 +61,13 @@ impl Server {
 			let arg = arg.clone();
 			let files = Arc::new(DashMap::default());
 			let progress = progress.clone();
+
 			async move {
+				let checkouts_path = server
+					.get_checkouts_directory_for_path(&id, arg.path.as_deref())
+					.await?;
 				server
-					.check_out_artifact_with_files(&id, &arg, files, &progress)
+					.check_out_artifact_with_files(&id, &arg, &checkouts_path, files, &progress)
 					.await
 			}
 		};
@@ -87,21 +91,11 @@ impl Server {
 		&self,
 		id: &tg::artifact::Id,
 		arg: &tg::artifact::checkout::Arg,
+		checkouts_path: &PathBuf,
 		files: Arc<DashMap<tg::file::Id, PathBuf, fnv::FnvBuildHasher>>,
 		progress: &crate::progress::Handle<tg::artifact::checkout::Output>,
 	) -> tg::Result<tg::artifact::checkout::Output> {
 		let artifact = tg::Artifact::with_id(id.clone());
-
-		// Bundle the artifact if requested.
-		let artifact = if arg.bundle {
-			artifact
-				.bundle(self)
-				.await
-				.map_err(|source| tg::error!(!source, "failed to bundle the artifact"))?
-		} else {
-			artifact
-		};
-
 		if let Some(path) = arg.path.clone() {
 			if !path.is_absolute() {
 				return Err(tg::error!(%path = path.display(), "the path must be absolute"));
@@ -137,12 +131,12 @@ impl Server {
 
 			// Perform the checkout.
 			let arg = InnerArg {
-				path: &path,
-				artifact: &artifact,
-				existing_artifact: existing_artifact.as_ref(),
-				depth: 0,
 				arg,
+				artifact: &artifact,
+				checkouts_path,
+				existing_artifact: existing_artifact.as_ref(),
 				files,
+				path: &path,
 				progress,
 			};
 			self.check_out_inner(arg).await?;
@@ -154,7 +148,7 @@ impl Server {
 		} else {
 			// Get the path in the checkouts directory.
 			let id = artifact.id(self).await?;
-			let path = self.checkouts_path().join(id.to_string());
+			let path = checkouts_path.join(id.to_string());
 			let artifact_path = self.artifacts_path().join(id.to_string());
 
 			// If there is already a file system object at the path, then return.
@@ -168,29 +162,27 @@ impl Server {
 				return Ok(output);
 			}
 
-			// If the VFS is enabled and `force` is `false`, then return.
-			if self.options.vfs.is_some() && !arg.force {
-				let output = tg::artifact::checkout::Output {
-					path: artifact_path,
-				};
-				return Ok(output);
-			}
-
 			// Create a tmp.
 			let tmp = Tmp::new(self);
 
 			// Perform the checkout to the tmp.
 			let files = Arc::new(DashMap::default());
 			let arg = InnerArg {
-				path: &tmp.path,
-				artifact: &artifact,
-				existing_artifact: None,
-				depth: 0,
 				arg,
+				artifact: &artifact,
+				checkouts_path,
+				existing_artifact: None,
 				files,
+				path: &tmp.path,
 				progress,
 			};
 			self.check_out_inner(arg).await?;
+
+			if let Some(parent) = path.parent() {
+				tokio::fs::create_dir_all(parent)
+					.await
+					.map_err(|source| tg::error!(!source, "failed to create output directory"))?;
+			}
 
 			// Move the checkout to the checkouts directory.
 			match tokio::fs::rename(&tmp, &path).await {
@@ -200,6 +192,10 @@ impl Server {
 					if matches!(error.raw_os_error(), Some(libc::ENOTEMPTY | libc::EEXIST)) =>
 				{
 					crate::util::fs::remove(&tmp).await.ok();
+				},
+				// If the checkout is on a different filesystem, perform a deep copy.
+				Err(ref error) if matches!(error.raw_os_error(), Some(libc::ENOSYS)) => {
+					todo!()
 				},
 
 				// Otherwise, return the error.
@@ -223,7 +219,7 @@ impl Server {
 		let InnerArg {
 			arg,
 			artifact,
-			depth,
+			checkouts_path,
 			existing_artifact,
 			files,
 			path,
@@ -245,7 +241,7 @@ impl Server {
 		let arg_ = InnerArg {
 			arg,
 			artifact,
-			depth,
+			checkouts_path,
 			existing_artifact,
 			files,
 			path,
@@ -294,12 +290,13 @@ impl Server {
 		let InnerArg {
 			arg,
 			artifact,
-			depth,
+			checkouts_path,
 			existing_artifact,
 			files,
 			path,
 			progress,
 		} = arg;
+
 		let directory = artifact
 			.try_unwrap_directory_ref()
 			.ok()
@@ -363,7 +360,7 @@ impl Server {
 					let arg = InnerArg {
 						arg,
 						artifact,
-						depth: depth + 1,
+						checkouts_path,
 						existing_artifact: existing_artifact.as_ref(),
 						files,
 						path: &entry_path,
@@ -385,12 +382,14 @@ impl Server {
 		let InnerArg {
 			arg,
 			artifact,
+			checkouts_path,
 			existing_artifact,
 			files,
 			path,
 			progress,
 			..
 		} = arg;
+
 		let file = artifact
 			.try_unwrap_file_ref()
 			.ok()
@@ -418,19 +417,20 @@ impl Server {
 			.try_collect::<BTreeSet<_>>()
 			.await
 			.map_err(|source| tg::error!(!source, "failed to get the file's dependencies"))?;
-		if arg.dependencies && !dependencies.is_empty() {
+
+		if arg.references && !dependencies.is_empty() {
 			dependencies
 				.iter()
 				.map(|artifact| async {
 					let arg = tg::artifact::checkout::Arg {
-						bundle: false,
 						force: false,
 						path: None,
-						dependencies: true,
+						references: true,
 					};
 					Box::pin(self.check_out_artifact_with_files(
 						artifact,
 						&arg,
+						checkouts_path,
 						files.clone(),
 						progress,
 					))
@@ -458,7 +458,7 @@ impl Server {
 		}
 
 		// Attempt to use the file from an internal checkout.
-		let internal_checkout_path = self.checkouts_path().join(id.to_string());
+		let internal_checkout_path = checkouts_path.join(id.to_string());
 		if arg.path.is_none() {
 			// If this checkout is internal, then create a hard link.
 			let result = tokio::fs::hard_link(&internal_checkout_path, path).await;
@@ -514,12 +514,13 @@ impl Server {
 		let InnerArg {
 			arg,
 			artifact,
-			depth,
+			checkouts_path,
 			existing_artifact,
 			files,
 			path,
 			progress,
 		} = arg;
+
 		let symlink = artifact
 			.try_unwrap_symlink_ref()
 			.ok()
@@ -536,35 +537,33 @@ impl Server {
 			None => (),
 		};
 
+		// Get the symlink's data.
+		let artifact = symlink.artifact(self).await?;
+		let path_ = symlink.path(self).await?;
+
 		// Check out the symlink's artifact if necessary.
-		if arg.dependencies {
-			if let Some(artifact) = symlink.artifact(self).await? {
-				if arg.path.is_some() {
-					return Err(tg::error!(
-						r#"cannot perform an external check out of a symlink with an artifact"#
-					));
-				}
+		if arg.references {
+			if let Some(artifact) = &artifact {
 				let id = artifact.id(self).await?;
 				let arg = tg::artifact::checkout::Arg::default();
-				Box::pin(self.check_out_artifact_with_files(&id, &arg, files, progress)).await?;
+				Box::pin(self.check_out_artifact_with_files(
+					&id,
+					&arg,
+					checkouts_path,
+					files,
+					progress,
+				))
+				.await?;
 			}
 		}
 
 		// Render the target.
 		let mut target = PathBuf::new();
-		let artifact = symlink.artifact(self).await?;
-		let path_ = symlink.path(self).await?;
-		if let Some(artifact) = artifact.as_ref() {
-			for _ in 0..depth {
-				target.push(std::path::Component::ParentDir);
-			}
-			target = target.join("../../.tangram/artifacts");
-			let id = &artifact.id(self).await?.to_string();
-			let component = std::path::Component::Normal(OsStr::new(id));
-			target.push(component);
+		if artifact.is_some() {
+			target.push(path.diff(checkouts_path).unwrap().parent().unwrap());
 		}
-		if let Some(path) = path_.as_ref() {
-			target = target.join(path.clone());
+		if let Some(path) = path_ {
+			target.push(path);
 		}
 
 		// Create the symlink.
@@ -573,6 +572,31 @@ impl Server {
 			.map_err(|source| tg::error!(!source, "failed to create the symlink"))?;
 
 		Ok(())
+	}
+}
+
+impl Server {
+	#[allow(clippy::unused_self, clippy::unused_async)]
+	async fn get_checkouts_directory_for_path(
+		&self,
+		artifact: &tg::artifact::Id,
+		output_path: Option<&Path>,
+	) -> tg::Result<PathBuf> {
+		let Some(output_path) = output_path else {
+			return Ok(self.checkouts_path());
+		};
+
+		if !output_path.is_absolute() {
+			return Err(tg::error!(%path = output_path.display(), "expected an absolute path"));
+		}
+
+		let checkouts_path = if let tg::artifact::Id::Directory(_) = artifact {
+			output_path.join(".tangram/artifacts")
+		} else {
+			output_path.parent().unwrap().join(".tangram/artifacts")
+		};
+
+		Ok(checkouts_path)
 	}
 }
 
@@ -634,5 +658,80 @@ impl Server {
 		let response = response.body(body).unwrap();
 
 		Ok(response)
+	}
+}
+
+impl Server {
+	async fn create_lockfile_from_artifact(&self, artifact: &tg::Artifact) -> tg::Result<tg::Lockfile> {
+		let mut nodes = Vec::new();
+		let mut visited = BTreeMap::new();
+		self.create_lockfile_from_artifact_inner(artifact, &mut nodes, &mut visited).await?;
+		let nodes = nodes.into_iter().map(Option::unwrap).collect();
+		Ok(tg::Lockfile { nodes })
+	}
+
+	async fn create_lockfile_from_artifact_inner(
+		&self, 
+		artifact: &tg::Artifact,
+		nodes: &mut Vec<Option<tg::lockfile::Node>>,
+		visited: &mut BTreeMap<tg::artifact::Id, usize>
+	) -> tg::Result<usize> {
+		let id = artifact.id(self).await?;
+		if let Some(visited) = visited.get(&id) {
+			return Ok(*visited);
+		}
+		let mut index = nodes.len();
+		nodes.push(None);
+		visited.insert(id, index);
+
+		let node = match artifact {
+			tg::Artifact::Directory(directory) => {
+				todo!()
+			},
+			tg::Artifact::File(file) => {
+				todo!()
+			},
+			tg::Artifact::Symlink(symlink) => {
+				todo!()
+			}
+		};
+
+
+
+		Ok(index)
+	}
+
+	async fn create_lockfile_nodes_from_graph(&self,
+		graph: &tg::Graph,
+		nodes: &mut Vec<Option<tg::lockfile::Node>>,
+		visited: &mut BTreeMap<tg::artifact::Id, usize>
+	) -> tg::Result<()> {
+		let object = graph.object(self).await?;
+		let mut indices = Vec::with_capacity(object.nodes.len());
+
+		// Assign indices.
+		for node in 0..object.nodes.len() {
+			let index = nodes.len();
+			nodes.push(None);
+			indices.push(index);
+		}
+
+		// Create nodes
+		for (old_index, node) in object.nodes.iter().enumerate() {
+			match node {
+				tg::graph::Node::Directory(directory) => {
+					
+				},
+				tg::graph::Node::File(file) => {
+
+				},
+				tg::graph::Node::Symlink(symlink) => {
+
+				},
+			}
+		}
+
+
+		Ok(())
 	}
 }
