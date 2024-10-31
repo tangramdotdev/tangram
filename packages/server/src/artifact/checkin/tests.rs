67d0b146b5db7dc69d290e10c53825ecb153cbda
David Yamnitsky <david@yamnitsky.com>
2024-10-31 09:45:17 -0400

    wip

--- packages/server/src/artifact/checkin/tests.rs
+++ packages/server/src/artifact/checkin/tests.rs
@@ -6,64 +6,88 @@ use tangram_client as tg;
 use tangram_futures::stream::TryStreamExt as _;
 use tangram_temp::{self as temp, artifact, Temp};
 
-type Result = std::result::Result<(), Box<dyn std::error::Error>>;
-
-#[tokio::test]
-async fn basic() -> Result {
-	let artifact = artifact!({
-		"directory": {
-			"README.md": "Hello, World!",
+macro_rules! test {
+	($artifact:expr, $path:expr, $f:expr,) => {
+		async {
+			let temp = Temp::new();
+			let options = Config::with_path(temp.path().to_owned());
+			let server = Server::start(options).await?;
+			let result = AssertUnwindSafe(async {
+				let directory = Temp::new();
+				$artifact
+					.to_path(directory.as_ref())
+					.await
+					.map_err(|source| tg::error!(!source, "failed to write the artifact"))?;
+				let arg = tg::artifact::checkin::Arg {
+					destructive: false,
+					deterministic: false,
+					ignore: true,
+					locked: false,
+					path: directory.as_ref().join($path),
+				};
+				let artifact = tg::Artifact::check_in(&server, arg).await?;
+				let object = tg::Object::from(artifact.clone());
+				object.load_recursive(&server).await?;
+				let value = tg::Value::from(artifact.clone());
+				let options = tg::value::print::Options {
+					recursive: true,
+					style: tg::value::print::Style::Pretty { indentation: "\t" },
+				};
+				let output = value.print(options);
+				$f(server.clone(), artifact, output).await?;
+				Ok::<_, tg::Error>(())
+			})
+			.catch_unwind()
+			.await;
+			server.stop();
+			server.wait().await;
+			result.unwrap()
 		}
-	});
-	let path = "directory";
-	test_checkin(artifact, path, |server, artifact| async { Ok(()) }).await
+	};
 }
 
-async fn test_checkin<F, Fut>(artifact: temp::Artifact, path: &str, assertions: F) -> Result
-where
-	F: FnOnce(&Server, &tg::Artifact) -> Fut,
-	Fut: Future<Output = Result>,
-{
-	test_server(|server| async move {
-		let directory = Temp::new();
-		artifact.to_path(directory.as_ref()).await?;
-		let arg = tg::artifact::checkin::Arg {
-			destructive: false,
-			deterministic: false,
-			ignore: true,
-			locked: false,
-			path: directory.as_ref().join(path),
-		};
-		let stream = server.check_in_artifact(arg).await?;
-		let output = pin!(stream)
-			.try_last()
-			.await?
-			.and_then(|event| event.try_unwrap_output().ok())
-			.ok_or_else(|| tg::error!("stream ended without output"))?;
-		let artifact = tg::Artifact::with_id(output.artifact);
-		tg::Object::from(artifact.clone())
-			.load_recursive(&server)
-			.await?;
-		let output = tg::Value::from(artifact.clone()).print(tg::value::print::Options {
-			recursive: true,
-			style: tg::value::print::Style::Pretty { indentation: "\t" },
-		});
-		assert_snapshot!(output);
-		Ok(())
-	})
+#[tokio::test]
+async fn readme() -> tg::Result<()> {
+	test!(
+		artifact!({
+			"directory": {
+				"README.md": "Hello, World!!",
+			}
+		}),
+		"directory",
+		|_, _, output| async move {
+			assert_snapshot!(output, @r#"
+   tg.directory({
+   	"README.md": tg.file({
+   		"contents": tg.leaf("Hello, World!!"),
+   	}),
+   })
+   "#);
+			Ok::<_, tg::Error>(())
+		},
+	)
 	.await
 }
 
-async fn test_server<F, Fut>(f: F) -> Result
-where
-	F: FnOnce(Server) -> Fut,
-	Fut: Future<Output = Result>,
-{
-	let temp = Temp::new();
-	let options = Config::with_path(temp.path().to_owned());
-	let server = Server::start(options).await?;
-	let result = AssertUnwindSafe((f)(server.clone())).catch_unwind().await;
-	server.stop();
-	server.wait().await;
-	result.unwrap()
+#[tokio::test]
+async fn default_module() -> tg::Result<()> {
+	test!(
+		artifact!({
+			"directory": {
+				"tangram.ts": "export default tg.target(() => {})",
+			}
+		}),
+		"directory",
+		|_, _, output| async move {
+			assert_snapshot!(output, @r#"
+   tg.directory({
+   	"tangram.ts": tg.file({
+   		"contents": tg.leaf("export default tg.target(() => {})"),
+   	}),
+   })
+   "#);
+			Ok::<_, tg::Error>(())
+		},
+	)
+	.await
 }
