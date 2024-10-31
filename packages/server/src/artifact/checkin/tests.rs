8fbb1474b01902b5abe344258663cfcad1e91608
David Yamnitsky <david@yamnitsky.com>
2024-10-31 09:54:12 -0400

    fix

--- packages/server/src/artifact/checkin/tests.rs
+++ packages/server/src/artifact/checkin/tests.rs
@@ -6,49 +6,9 @@ use tangram_client as tg;
 use tangram_futures::stream::TryStreamExt as _;
 use tangram_temp::{self as temp, artifact, Temp};
 
-macro_rules! test {
-	($artifact:expr, $path:expr, $f:expr,) => {
-		async {
-			let temp = Temp::new();
-			let options = Config::with_path(temp.path().to_owned());
-			let server = Server::start(options).await?;
-			let result = AssertUnwindSafe(async {
-				let directory = Temp::new();
-				$artifact
-					.to_path(directory.as_ref())
-					.await
-					.map_err(|source| tg::error!(!source, "failed to write the artifact"))?;
-				let arg = tg::artifact::checkin::Arg {
-					destructive: false,
-					deterministic: false,
-					ignore: true,
-					locked: false,
-					path: directory.as_ref().join($path),
-				};
-				let artifact = tg::Artifact::check_in(&server, arg).await?;
-				let object = tg::Object::from(artifact.clone());
-				object.load_recursive(&server).await?;
-				let value = tg::Value::from(artifact.clone());
-				let options = tg::value::print::Options {
-					recursive: true,
-					style: tg::value::print::Style::Pretty { indentation: "\t" },
-				};
-				let output = value.print(options);
-				$f(server.clone(), artifact, output).await?;
-				Ok::<_, tg::Error>(())
-			})
-			.catch_unwind()
-			.await;
-			server.stop();
-			server.wait().await;
-			result.unwrap()
-		}
-	};
-}
-
 #[tokio::test]
 async fn readme() -> tg::Result<()> {
-	test!(
+	test(
 		artifact!({
 			"directory": {
 				"README.md": "Hello, World!!",
@@ -71,7 +31,7 @@ async fn readme() -> tg::Result<()> {
 
 #[tokio::test]
 async fn default_module() -> tg::Result<()> {
-	test!(
+	test(
 		artifact!({
 			"directory": {
 				"tangram.ts": "export default tg.target(() => {})",
@@ -90,4 +50,44 @@ async fn default_module() -> tg::Result<()> {
 		},
 	)
 	.await
+}
+
+async fn test<F, Fut>(artifact: temp::Artifact, path: &str, assertions: F) -> tg::Result<()>
+where
+	F: FnOnce(Server, tg::Artifact, String) -> Fut,
+	Fut: Future<Output = tg::Result<()>>,
+{
+	let temp = Temp::new();
+	let options = Config::with_path(temp.path().to_owned());
+	let server = Server::start(options).await?;
+	let result = AssertUnwindSafe(async {
+		let directory = Temp::new();
+		artifact
+			.to_path(directory.as_ref())
+			.await
+			.map_err(|source| tg::error!(!source, "failed to write the artifact"))?;
+		let arg = tg::artifact::checkin::Arg {
+			destructive: false,
+			deterministic: false,
+			ignore: true,
+			locked: false,
+			path: directory.as_ref().join(path),
+		};
+		let artifact = tg::Artifact::check_in(&server, arg).await?;
+		let object = tg::Object::from(artifact.clone());
+		object.load_recursive(&server).await?;
+		let value = tg::Value::from(artifact.clone());
+		let options = tg::value::print::Options {
+			recursive: true,
+			style: tg::value::print::Style::Pretty { indentation: "\t" },
+		};
+		let output = value.print(options);
+		(assertions)(server.clone(), artifact, output).await?;
+		Ok::<_, tg::Error>(())
+	})
+	.catch_unwind()
+	.await;
+	server.stop();
+	server.wait().await;
+	result.unwrap()
 }
