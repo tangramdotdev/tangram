7aeba870719d2e1700f98f08dee154e4510d248e
Isabella Tromba <isabella.tromba@gmail.com>
2024-12-29 12:52:12 -0500

    feat: add support for tgvn output on get

--- packages/cli/src/get.rs
+++ packages/cli/src/get.rs
@@ -9,6 +9,12 @@ use tangram_either::Either;
 pub struct Args {
 	#[arg(index = 1)]
 	pub reference: tg::Reference,
+	#[arg(long)]
+	pub format: Option<String>,
+	#[arg(long, requires = "format", default_value = "false")]
+	pub pretty: bool,
+	#[arg(long, requires = "format", default_value = "false")]
+	pub recursive: bool,
 }
 
 impl Cli {
@@ -30,6 +36,33 @@ impl Cli {
 				Either::Right(object)
 			},
 		};
+		if args.format.as_deref() == Some("tgvn") {
+			let pretty = args.pretty;
+			let recursive = args.recursive;
+			match item {
+				Either::Left(build) => {
+					tg::error!("expected an object")
+				},
+				Either::Right(object) => {
+					let options = tg::value::print::Options {
+						recursive: true,
+						style: if pretty {
+							tg::value::print::Style::Pretty { indentation: "  " }
+						} else {
+							tg::value::print::Style::Compact
+						},
+					};
+					if recursive {
+						object.load_recursive(&handle).await?;
+					} else {
+						object.load(&handle).await?;
+					}
+					let value = tg::Value::from(object);
+					print!("{}", value.print(options));
+				},
+			}
+			return Ok(());
+		}
 		let item = match item {
 			Either::Left(build) => Either::Left(build.id().clone()),
 			Either::Right(object) => Either::Right(object.id(&handle).await?.clone()),
