96480b26ff35ee6aee2138e625e73a2ded75140c
David Yamnitsky <david@yamnitsky.com>
2025-01-09 11:39:17 -0500

    fix: canary script

--- scripts/canary.ts
+++ scripts/canary.ts
@@ -3,28 +3,54 @@ import fs from "node:fs";
 import path from "node:path";
 
 await $`rm -rf release && mkdir release`;
+
 const hash = (await $`git rev-parse HEAD`).text().trim();
-const targets = [
-	"aarch64-apple-darwin",
-	"aarch64-unknown-linux-gnu",
-	"x86_64-apple-darwin",
-	"x86_64-unknown-linux-gnu",
-];
-await $`
-	TANGRAM_CLI_COMMIT_HASH=${hash}
-	cargo build
-		--release
-		--target ${targets[0]}
-		--target ${targets[1]}
-		--target ${targets[2]}
-		--target ${targets[3]}
-`;
-for (const target of targets) {
-	const releasePath = `target/${target}/release`;
+
+await $`cargo build --release --target aarch64-apple-darwin --target aarch64-unknown-linux-gnu --target x86_64-apple-darwin --target x86_64-unknown-linux-gnu`.env(
+	{
+		...process.env,
+		TANGRAM_CLI_COMMIT_HASH: hash,
+	},
+);
+
+let releasePath, archiveName;
+
+releasePath = `target/aarch64-apple-darwin/release`;
+try {
 	await fs.promises.symlink("tangram", path.join(releasePath, "tg"));
-	const archiveName = `tangram_${target.replace(/-/g, "_")}.tar.gz`;
-	await $`tar -czf release/${archiveName} -C ${releasePath} tangram tg`;
+} catch (error) {
+	if (error.code !== "EEXIST") throw error;
 }
+archiveName = `tangram_aarch64-darwin.tar.gz`;
+await $`tar -czf release/${archiveName} -C ${releasePath} tangram tg`;
+
+releasePath = `target/aarch64-unknown-linux-gnu/release`;
+try {
+	await fs.promises.symlink("tangram", path.join(releasePath, "tg"));
+} catch (error) {
+	if (error.code !== "EEXIST") throw error;
+}
+archiveName = `tangram_aarch64-linux.tar.gz`;
+await $`tar -czf release/${archiveName} -C ${releasePath} tangram tg`;
+
+releasePath = `target/x86_64-apple-darwin/release`;
+try {
+	await fs.promises.symlink("tangram", path.join(releasePath, "tg"));
+} catch (error) {
+	if (error.code !== "EEXIST") throw error;
+}
+archiveName = `tangram_x86_64-darwin.tar.gz`;
+await $`tar -czf release/${archiveName} -C ${releasePath} tangram tg`;
+
+releasePath = `target/x86_64-unknown-linux-gnu/release`;
+try {
+	await fs.promises.symlink("tangram", path.join(releasePath, "tg"));
+} catch (error) {
+	if (error.code !== "EEXIST") throw error;
+}
+archiveName = `tangram_x86_64-linux.tar.gz`;
+await $`tar -czf release/${archiveName} -C ${releasePath} tangram tg`;
+
 await $`gh release upload canary release/* --clobber`;
 await $`git tag canary -f`;
 await $`git push origin tag canary -f`;
