778d8a0912368cdc3b849df2ff88903ea00e3d12
Isabella Tromba <isabella.tromba@gmail.com>
2025-02-02 10:06:34 -0500

    fix: make clean a single query postgres

--- packages/server/src/clean.rs
+++ packages/server/src/clean.rs
@@ -1,9 +1,9 @@
 use super::Server;
-use indoc::formatdoc;
+use indoc::{formatdoc, indoc};
 use tangram_client as tg;
 use tangram_database::{self as db, prelude::*};
-use tangram_http::{response::builder::Ext as _, Body};
-use time::format_description::well_known::Rfc3339;
+use tangram_either::Either;
+use tangram_http::{outgoing::response::Ext as _, Incoming, Outgoing};
 
 impl Server {
 	pub async fn clean(&self) -> tg::Result<()> {
@@ -17,176 +17,320 @@ impl Server {
 				tg::error!(source = error, "failed to recreate the temporary directory")
 			})?;
 
-		// Get a database connection.
-		let mut connection = self
-			.database
-			.write_connection()
-			.await
-			.map_err(|source| tg::error!(!source, "failed to get a database connection"))?;
-
-		// Remove processes.
-		loop {
-			// Begin a transaction.
-			let transaction = connection
-				.transaction()
-				.await
-				.map_err(|source| tg::error!(!source, "failed to begin a transaction"))?;
-			let max_touched_at = (time::OffsetDateTime::now_utc()
-				- self.config().advanced.garbage_collection_grace_period)
-				.format(&Rfc3339)
-				.unwrap();
-
-			// Get a process to remove.
-			let p = transaction.p();
-			let statement = formatdoc!(
-				"
-					select id
-					from processes
-					where reference_count = 0 and touched_at <= {p}1
-					limit 100;
-				"
-			);
-			let params = db::params![max_touched_at];
-			let processes = transaction
-				.query_all_value_into::<tg::process::Id>(statement.into(), params)
-				.await
-				.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
-
-			// If there are no processes, then break.
-			if processes.is_empty() {
-				break;
-			}
-
-			for id in processes {
-				// Remove the process.
-				let p = transaction.p();
-				let statement = formatdoc!(
-					"
-						delete from processes
-						where id = {p}1;
-					"
-				);
-				let params = db::params![id];
-				transaction
-					.execute(statement.into(), params)
+		match &self.database {
+			Either::Left(database) => {
+				// Get a database connection.
+				let mut connection = database
+					.write_connection()
 					.await
-					.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
+					.map_err(|source| tg::error!(!source, "failed to get a database connection"))?;
 
-				// Remove the process children.
-				let p = transaction.p();
-				let statement = formatdoc!(
-					"
-						delete from process_children
-						where process = {p}1;
-					"
-				);
-				let params = db::params![id];
-				transaction
-					.execute(statement.into(), params)
+				// Remove processes.
+				loop {
+					// Begin a transaction.
+					let transaction = connection
+						.transaction()
+						.await
+						.map_err(|source| tg::error!(!source, "failed to begin a transaction"))?;
+
+					// Get a process to remove.
+					let statement = indoc!(
+						"
+							select id
+							from processes
+							where (
+								select count(*) = 0
+								from process_children
+								where child = processes.id
+							) and (
+								select count(*) = 0
+								from tags
+								where item = processes.id
+							)
+							limit 100;
+						"
+					);
+					let params = db::params![];
+					let processes = transaction
+						.query_all_value_into::<tg::process::Id>(statement.into(), params)
+						.await
+						.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
+
+					// If there are no processes, then break.
+					if processes.is_empty() {
+						break;
+					}
+
+					for id in processes {
+						// Remove the process.
+						let p = transaction.p();
+						let statement = formatdoc!(
+							"
+								delete from processes
+								where id = {p}1;
+							"
+						);
+						let params = db::params![id];
+						transaction
+							.execute(statement.into(), params)
+							.await
+							.map_err(|source| {
+								tg::error!(!source, "failed to execute the statement")
+							})?;
+
+						// Remove the process children.
+						let p = transaction.p();
+						let statement = formatdoc!(
+							"
+								delete from process_children
+								where process = {p}1;
+							"
+						);
+						let params = db::params![id];
+						transaction
+							.execute(statement.into(), params)
+							.await
+							.map_err(|source| {
+								tg::error!(!source, "failed to execute the statement")
+							})?;
+
+						// Remove the process objects.
+						let p = transaction.p();
+						let statement = formatdoc!(
+							"
+								delete from process_objects
+								where process = {p}1;
+							"
+						);
+						let params = db::params![id];
+						transaction
+							.execute(statement.into(), params)
+							.await
+							.map_err(|source| {
+								tg::error!(!source, "failed to execute the statement")
+							})?;
+					}
+
+					// Commit the transaction.
+					transaction.commit().await.map_err(|source| {
+						tg::error!(!source, "failed to commit the transaction")
+					})?;
+				}
+
+				// Remove objects.
+				loop {
+					// Begin a transaction.
+					let transaction = connection
+						.transaction()
+						.await
+						.map_err(|source| tg::error!(!source, "failed to begin a transaction"))?;
+
+					// Get objects to remove.
+					let statement = formatdoc!(
+						"
+							select id
+							from objects
+							where (
+								select count(*) = 0
+								from object_children
+								where child = objects.id
+							) and (
+								select count(*) = 0
+								from process_objects
+								where object = objects.id
+							) and (
+								select count(*) = 0
+								from tags
+								where item = objects.id
+							)
+							limit 100;
+						"
+					);
+					let params = db::params![];
+					let objects = transaction
+						.query_all_value_into::<tg::object::Id>(statement.into(), params)
+						.await
+						.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
+
+					// If there are no objects, then break.
+					if objects.is_empty() {
+						break;
+					}
+
+					for id in objects {
+						// Remove the object.
+						let p = transaction.p();
+						let statement = formatdoc!(
+							"
+								delete from objects
+								where id = {p}1;
+							"
+						);
+						let params = db::params![id];
+						transaction
+							.execute(statement.into(), params)
+							.await
+							.map_err(|source| {
+								tg::error!(!source, "failed to execute the statement")
+							})?;
+
+						// Remove the object children.
+						let p = transaction.p();
+						let statement = formatdoc!(
+							"
+								delete from object_children
+								where object = {p}1;
+							"
+						);
+						let params = db::params![id];
+						transaction
+							.execute(statement.into(), params)
+							.await
+							.map_err(|source| {
+								tg::error!(!source, "failed to execute the statement")
+							})?;
+					}
+
+					// Commit the transaction.
+					transaction.commit().await.map_err(|source| {
+						tg::error!(!source, "failed to commit the transaction")
+					})?;
+				}
+
+				Ok(())
+			},
+			Either::Right(database) => {
+				// Get a database connection.
+				let mut connection = database
+					.write_connection()
 					.await
-					.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
+					.map_err(|source| tg::error!(!source, "failed to get a database connection"))?;
 
-				// Remove the process objects.
-				let p = transaction.p();
-				let statement = formatdoc!(
-					"
-						delete from process_objects
-						where process = {p}1;
-					"
-				);
-				let params = db::params![id];
-				transaction
-					.execute(statement.into(), params)
-					.await
-					.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
-			}
+				// Remove processes.
+				loop {
+					// Begin a transaction.
+					let transaction = connection
+						.transaction()
+						.await
+						.map_err(|source| tg::error!(!source, "failed to begin a transaction"))?;
 
-			// Commit the transaction.
-			transaction
-				.commit()
-				.await
-				.map_err(|source| tg::error!(!source, "failed to commit the transaction"))?;
+					// Get a process to remove.
+					let statement = indoc!(
+						"
+						with eligible_processes as (
+							select id
+							from processes
+							where (
+								select count(*) = 0
+								from process_children
+								where child = processes.id
+							) and (
+								select count(*) = 0
+								from tags
+								where item = processes.id
+							)
+							limit 100
+						), 
+						deleted_processes as (
+							delete from processes
+							where id in (select id from eligible_processes)
+						),
+						deleted_process_children as (
+							delete from process_children
+							where process in (select id from eligible_processes) 
+						),
+						deleted_process_objects as (
+							delete from process_objects
+							where process in (select id from eligible_processes)
+						)
+						select count(*) from eligible_processes;
+						"
+					);
+					let params = db::params![];
+					let processes = transaction
+						.query_one_value_into::<u64>(statement.into(), params)
+						.await
+						.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
+
+					// If there are no processes, then break.
+					if processes == 0 {
+						break;
+					}
+
+					// Commit the transaction.
+					transaction.commit().await.map_err(|source| {
+						tg::error!(!source, "failed to commit the transaction")
+					})?;
+				}
+
+				// Remove objects.
+				loop {
+					// Begin a transaction.
+					let transaction = connection
+						.transaction()
+						.await
+						.map_err(|source| tg::error!(!source, "failed to begin a transaction"))?;
+
+					// Get objects to remove.
+					let statement = formatdoc!(
+						"
+						with eligible_objects as (
+							select id
+							from objects
+							where (
+								select count(*) = 0
+								from object_children
+								where child = objects.id
+							) and (
+								select count(*) = 0
+								from process_objects
+								where object = objects.id
+							) and (
+								select count(*) = 0
+								from tags
+								where item = objects.id
+							)
+							limit 100
+						), 
+						deleted_objects as (
+							delete from objects
+							where id in (select id from eligible_objects)
+						),
+						deleted_object_children as (
+							delete from object_children
+							where object in (select id from eligible_objects)
+						)
+						select count(*) from eligible_objects;
+						"
+					);
+					let params = db::params![];
+					let objects = transaction
+						.query_one_value_into::<u64>(statement.into(), params)
+						.await
+						.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
+
+					// If there are no objects, then break.
+					if objects == 0 {
+						break;
+					}
+
+					// Commit the transaction.
+					transaction.commit().await.map_err(|source| {
+						tg::error!(!source, "failed to commit the transaction")
+					})?;
+				}
+
+				Ok(())
+			},
 		}
-
-		// Remove objects.
-		loop {
-			// Begin a transaction.
-			let transaction = connection
-				.transaction()
-				.await
-				.map_err(|source| tg::error!(!source, "failed to begin a transaction"))?;
-			let max_touched_at = (time::OffsetDateTime::now_utc()
-				- self.config().advanced.garbage_collection_grace_period)
-				.format(&Rfc3339)
-				.unwrap();
-
-			// Get objects to remove.
-			let p = transaction.p();
-			let statement = formatdoc!(
-				"
-					select id
-					from objects
-					where reference_count = 0 and touched_at <= {p}1
-					limit 100;
-				"
-			);
-			let params = db::params![max_touched_at];
-			let objects = transaction
-				.query_all_value_into::<tg::object::Id>(statement.into(), params)
-				.await
-				.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
-
-			// If there are no objects, then break.
-			if objects.is_empty() {
-				break;
-			}
-
-			for id in objects {
-				// Remove the object.
-				let p = transaction.p();
-				let statement = formatdoc!(
-					"
-						delete from objects
-						where id = {p}1;
-					"
-				);
-				let params = db::params![id];
-				transaction
-					.execute(statement.into(), params)
-					.await
-					.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
-
-				// Remove the object children.
-				let p = transaction.p();
-				let statement = formatdoc!(
-					"
-						delete from object_children
-						where object = {p}1;
-					"
-				);
-				let params = db::params![id];
-				transaction
-					.execute(statement.into(), params)
-					.await
-					.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
-			}
-
-			// Commit the transaction.
-			transaction
-				.commit()
-				.await
-				.map_err(|source| tg::error!(!source, "failed to commit the transaction"))?;
-		}
-
-		Ok(())
 	}
 }
 
 impl Server {
 	pub(crate) async fn handle_server_clean_request<H>(
 		handle: &H,
-		_request: http::Request<Body>,
-	) -> tg::Result<http::Response<Body>>
+		_request: http::Request<Incoming>,
+	) -> tg::Result<http::Response<Outgoing>>
 	where
 		H: tg::Handle,
 	{
