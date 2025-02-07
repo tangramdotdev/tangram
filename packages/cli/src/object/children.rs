483c788161a9a6730ab58650342011a0077d5575
David Yamnitsky <david@yamnitsky.com>
2025-02-07 12:52:32 -0500

    fix(cli/children): deduplicate children

--- packages/cli/src/object/children.rs
+++ packages/cli/src/object/children.rs
@@ -13,13 +13,11 @@ impl Cli {
 	pub async fn command_object_children(&self, args: Args) -> tg::Result<()> {
 		let handle = self.handle().await?;
 
-		// Get the object.
+		// Print the children.
 		let object = tg::Object::with_id(args.object);
-
-		// Get the children.
-		let children = object.children(&handle).await?;
-		for child in children {
-			println!("{}", child.id(&handle).await?);
+		let data = object.data(&handle).await?;
+		for child in data.children() {
+			println!("{child}");
 		}
 
 		Ok(())
