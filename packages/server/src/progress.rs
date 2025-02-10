fc9c5c96ddb668639126588d08d35588c20a5ed9
Ben Lovy <ben@deciduously.com>
2025-02-10 09:55:29 -0500

    use AtomicBool

--- packages/server/src/progress.rs
+++ packages/server/src/progress.rs
@@ -1,7 +1,10 @@
 use futures::{future, stream, Stream, StreamExt as _};
 use indexmap::IndexMap;
 use std::{
-	sync::{atomic::AtomicU64, Arc, Mutex, RwLock},
+	sync::{
+		atomic::{AtomicBool, AtomicU64},
+		Arc, Mutex, RwLock,
+	},
 	time::Duration,
 };
 use tangram_client as tg;
@@ -13,7 +16,7 @@ pub struct Handle<T> {
 	indicators: Arc<RwLock<IndexMap<String, Indicator>>>,
 	receiver: async_channel::Receiver<tg::Result<tg::progress::Event<T>>>,
 	sender: async_channel::Sender<tg::Result<tg::progress::Event<T>>>,
-	sent_once: Arc<RwLock<bool>>,
+	indicators_sent: Arc<AtomicBool>,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
@@ -29,12 +32,12 @@ impl<T> Handle<T> {
 	pub fn new() -> Self {
 		let (sender, receiver) = async_channel::unbounded();
 		let indicators = Arc::new(RwLock::new(IndexMap::new()));
-		let sent_once = Arc::new(RwLock::new(false));
+		let indicators_sent = Arc::new(AtomicBool::new(false));
 		Self {
 			indicators,
 			receiver,
 			sender,
-			sent_once,
+			indicators_sent,
 		}
 	}
 
@@ -82,9 +85,14 @@ impl<T> Handle<T> {
 	}
 
 	pub fn finish(&self, name: &str) {
-		if let Some(indicator) = self.indicators.read().unwrap().get(name) {
-			let event = Self::get_indicator_update_event(indicator);
-			self.sender.try_send(Ok(event)).ok();
+		let indicators_sent = self
+			.indicators_sent
+			.load(std::sync::atomic::Ordering::Relaxed);
+		if indicators_sent {
+			if let Some(indicator) = self.indicators.read().unwrap().get(name) {
+				let event = Self::get_indicator_update_event(indicator);
+				self.sender.try_send(Ok(event)).ok();
+			}
 		}
 		self.indicators.write().unwrap().shift_remove(name);
 	}
@@ -101,11 +109,11 @@ impl<T> Handle<T> {
 	pub fn stream(&self) -> impl Stream<Item = tg::Result<tg::progress::Event<T>>> + use<T> {
 		let indicators = self.indicators.clone();
 		let receiver = self.receiver.clone();
-		let sent_once = self.sent_once.clone();
+		let indicators_sent = self.indicators_sent.clone();
 		let interval = Duration::from_millis(100);
 		let interval = tokio::time::interval(interval);
 		let updates = IntervalStream::new(interval).skip(1).flat_map(move |_| {
-			*sent_once.write().unwrap() = true;
+			indicators_sent.store(true, std::sync::atomic::Ordering::Relaxed);
 			stream::iter(Self::get_indicator_update_events(&indicators))
 		});
 		stream::select(receiver, updates).take_while_inclusive(|event| {
