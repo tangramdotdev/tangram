6b7c0d6182cea9b797ed041e95dfa2ccb7585216
David Yamnitsky <david@yamnitsky.com>
2025-02-28 12:24:16 -0500

    fix: only link to fdb if the feature is enabled

--- packages/cli/build.rs
+++ packages/cli/build.rs
@@ -3,9 +3,11 @@ use std::process::Command;
 
 fn main() {
 	println!("cargo:rerun-if-changed=build.rs");
+	#[cfg(feature = "foundationdb")]
 	fdb();
 }
 
+#[cfg(feature = "foundationdb")]
 fn fdb() {
 	let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
 	let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
