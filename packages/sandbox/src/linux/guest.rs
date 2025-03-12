d5cbec58f54aeacd5e76202939a396682881001f
m-hilgendorf <mike@hilgendorf.audio>
2025-03-12 14:43:01 -0500

    wip

---
+++ packages/sandbox/src/linux/guest.rs
@@ -0,0 +1,133 @@
+use std::os::fd::AsRawFd;
+use crate::{abort_errno, common::redirect_stdio};
+use super::Context;
+
+pub fn main(mut context: Context) -> ! {
+	unsafe {
+		// Redirect stdio.
+		redirect_stdio(&mut context.stdin, &mut context.stdout, &mut context.stderr);
+
+		// Wait for the notification from the host process to continue.
+		let mut notification = 0u8;
+		let ret = libc::recv(
+			context.socket.as_raw_fd(),
+			std::ptr::addr_of_mut!(notification).cast(),
+			std::mem::size_of_val(&notification),
+			0,
+		);
+		if ret == -1 {
+			abort_errno!(
+				"the guest process failed to receive the notification from the host process to continue"
+			);
+		}
+		assert_eq!(notification, 1);
+
+		// If requested to spawn in a chroot, perform the mounts and chroot.
+		if context.root.is_some() {
+			mount_and_chroot(&mut context);
+		}
+
+		// Set the working directory.
+		let ret = libc::chdir(context.cwd.as_ptr());
+		if ret == -1 {
+			abort_errno!("failed to set the working directory");
+		}
+
+		// Finally, exec the process.
+		libc::execve(
+			context.executable.as_ptr(),
+			context.argv.as_ptr().cast(),
+			context.envp.as_ptr().cast(),
+		);
+
+		abort_errno!("execve failed")
+	}
+}
+
+fn mount_and_chroot(context: &mut Context) {
+	let root = context.root.as_ref().unwrap();
+	unsafe {
+		for mount in &context.mounts {
+			let source = mount.source.as_ptr();
+			let target = mount.target.as_ptr();
+			let fstype = mount
+				.fstype
+				.as_ref()
+				.map_or_else(std::ptr::null, |value| value.as_ptr());
+			let flags = mount.flags;
+			let data = mount
+				.data
+				.as_ref()
+				.map_or_else(std::ptr::null, Vec::as_ptr)
+				.cast();
+			let ret = libc::mount(source, target, fstype, flags, data);
+			if ret == -1 {
+				abort_errno!(
+					r#"failed to mount "{}" to "{}""#,
+					mount.source.to_str().unwrap(),
+					mount.target.to_str().unwrap(),
+				);
+			}
+			if mount.readonly {
+				let ret = libc::mount(
+					source,
+					target,
+					fstype,
+					flags | libc::MS_RDONLY | libc::MS_REMOUNT,
+					data,
+				);
+				if ret == -1 {
+					abort_errno!(
+						r#"failed to mount "{}" to "{}""#,
+						mount.source.to_str().unwrap(),
+						mount.target.to_str().unwrap(),
+					);
+				}
+			}
+		}
+
+		// Mount the root.
+		let ret = libc::mount(
+			root.as_ptr(),
+			root.as_ptr(),
+			std::ptr::null(),
+			libc::MS_BIND | libc::MS_PRIVATE | libc::MS_REC,
+			std::ptr::null(),
+		);
+		if ret == -1 {
+			abort_errno!("failed to mount the root");
+		}
+
+		// Change the working directory to the pivoted root.
+		if let Some(root) = &context.root {
+			let ret = libc::chdir(root.as_ptr());
+			if ret == -1 {
+				abort_errno!("failed to change directory to the root");
+			}
+		}
+
+		// Pivot the root.
+		let ret = libc::syscall(libc::SYS_pivot_root, c".".as_ptr(), c".".as_ptr());
+		if ret == -1 {
+			abort_errno!("failed to pivot the root");
+		}
+
+		// Unmount the root.
+		let ret = libc::umount2(c".".as_ptr().cast(), libc::MNT_DETACH);
+		if ret == -1 {
+			abort_errno!("failed to unmount the root");
+		}
+
+		// Remount the root as read-only.
+		let ret = libc::mount(
+			std::ptr::null(),
+			c"/".as_ptr().cast(),
+			std::ptr::null(),
+			libc::MS_BIND | libc::MS_PRIVATE | libc::MS_RDONLY | libc::MS_REC | libc::MS_REMOUNT,
+			std::ptr::null(),
+		);
+		if ret == -1 {
+			abort_errno!("failed to remount the root as read-only");
+		}
+	}
+}
