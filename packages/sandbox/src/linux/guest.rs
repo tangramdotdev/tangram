5fb79a91d5a1f7b6e69e778ed94b03872f60fe57
Mike Hilgendorf <mike@hilgendorf.audio>
2025-03-21 16:40:13 -0500

    fix: libc::c_char

--- packages/sandbox/src/linux/guest.rs
+++ packages/sandbox/src/linux/guest.rs
@@ -131,9 +131,9 @@ fn mount_and_chroot(context: &mut Context) {
 
 fn create_mountpoint_if_not_exists(source: &CString, target: &mut CString) {
 	unsafe {
-		const BACKSLASH: u8 = b'\\';
-		const SLASH: u8 = b'/';
-		const NULL: u8 = 0;
+		const BACKSLASH: libc::c_char = b'\\' as _;
+		const SLASH: libc::c_char = b'/' as _;
+		const NULL: libc::c_char = 0;
 
 		// Determine if the target is a directory or not.
 		let is_dir = 'a: {
