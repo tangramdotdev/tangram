c34569224f41347ffeb7db9b8586462ffdb9ad62
Ben Lovy <ben@deciduously.com>
2025-04-03 11:00:04 -0400

    Revert "chore(tangram.ts): remove runtime components"
    This reverts commit 1bb31eceaa7380b8ce52dd10fad43b602a1f2288.

--- tangram.ts
+++ tangram.ts
@@ -2,18 +2,36 @@ import bun from "bun" with { path: "../packages/packages/bun" };
 import { cargo } from "rust" with { path: "../packages/packages/rust" };
 import * as std from "std" with { path: "../packages/packages/std" };
 
-import source from "." with { type: "directory" };
+import cargoToml from "./Cargo.toml" with { type: "file" };
+import cargoLock from "./Cargo.lock" with { type: "file" };
+import biomeJson from "./biome.json" with { type: "file" };
+import bunLockb from "./bun.lockb" with { type: "file" };
+import packageJson from "./package.json" with { type: "file" };
+import packages from "./packages" with { type: "directory" };
+
+export const source = tg.command(() =>
+	tg.directory({
+		"Cargo.toml": cargoToml,
+		"Cargo.lock": cargoLock,
+		"biome.json": biomeJson,
+		"bun.lockb": bunLockb,
+		"package.json": packageJson,
+		packages: packages,
+	}),
+);
 
 export default tg.command(async () => {
 	const host = std.triple.host();
-	const cargoLock = await source.get("Cargo.lock").then(tg.File.expect);
 	const bunArtifact = bun({ host });
-	const env = std.env.arg(bunArtifact, librustyv8(cargoLock, host));
+	const env = std.env.arg(
+		bunArtifact,
+		librustyv8(cargoLock, host),
+		linuxRuntimeComponents(),
+	);
 	const output = cargo.build({
 		buildInTree: true,
-		checksum: "any",
-		network: true,
-		source,
+		checksum: "unsafe",
+		source: source(),
 		env,
 	});
 	return tg.directory(output, {
@@ -32,14 +50,13 @@ export const librustyv8 = tg.command(
 		} else {
 			throw new Error(`unsupported host ${host}`);
 		}
-		const checksum = "any";
+		const checksum = "unsafe";
 		const file = `librusty_v8_release_${std.triple.arch(host)}-${os}.a.gz`;
 		const version = await getRustyV8Version(lockfile);
 		const lib = await std
 			.download({
 				checksum,
 				decompress: true,
-				extract: false,
 				url: `https://github.com/denoland/rusty_v8/releases/download/v${version}/${file}`,
 			})
 			.then(tg.File.expect);
@@ -65,3 +82,29 @@ const getRustyV8Version = async (lockfile: tg.File) => {
 type CargoLock = {
 	package: Array<{ name: string; version: string }>;
 };
+
+export const linuxRuntimeComponents = tg.command(async () => {
+	const version = "v2024.10.03";
+	const urlBase = `https://github.com/tangramdotdev/bootstrap/releases/download/${version}`;
+
+	const checksums: { [key: string]: tg.Checksum } = {
+		["DASH_AARCH64_LINUX"]:
+			"sha256:d1e6ed42b0596507ebfa9ce231e2f42cc67f823cc56c0897c126406004636ce7",
+		["DASH_X86_64_LINUX"]:
+			"sha256:d23258e559012dc66cc82d9def66b51e9c41f9fb88f8e9e6a5bd19d231028a64",
+		["ENV_AARCH64_LINUX"]:
+			"sha256:b2985354036c4deea9b107f099d853ac2d7c91a095dc285922f6dab72ae1474c",
+		["ENV_X86_64_LINUX"]:
+			"sha256:fceb5be5a7d6f59a026817ebb17be2bcc294d753f1528cbc921eb9015b9ff87b",
+	};
+	return Object.fromEntries(
+		await Promise.all(
+			Object.entries(checksums).map(async ([name, checksum]) => {
+				const file = await tg
+					.download(`${urlBase}/${name.toLowerCase()}.tar.zst`, checksum)
+					.then((blob) => tg.file(blob));
+				return [name, file];
+			}),
+		),
+	);
+});
