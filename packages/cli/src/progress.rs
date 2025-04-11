b5d10c3bf9cf547a53d6de0f67ff2ac11a073ae1
m-hilgendorf <mike@hilgendorf.audio>
2025-04-11 11:08:32 -0500

    fix: progress stream rendering
    - encapsulate the progress display's "state"
    - refactor into handling events with a timeout
    - fix line clearing

--- packages/cli/src/progress.rs
+++ packages/cli/src/progress.rs
@@ -11,12 +11,18 @@ use std::{
 use tangram_client as tg;
 use tangram_futures::stream::TryExt as _;
 
+struct State {
+	indicators: IndexMap<String, tg::progress::Indicator>,
+	lines: Option<u16>,
+	tty: std::io::Stderr,
+}
+
 impl Cli {
 	pub async fn render_progress_stream<T>(
 		&self,
 		stream: impl Stream<Item = tg::Result<tg::progress::Event<T>>>,
 	) -> tg::Result<T> {
-		let mut tty = std::io::stderr();
+		let tty = std::io::stderr();
 		let mut stream = pin!(stream);
 
 		if !tty.is_terminal() {
@@ -27,143 +33,173 @@ impl Cli {
 				.ok_or_else(|| tg::error!("stream ended without output"));
 		}
 
-		let mut indicators = IndexMap::new();
-		let mut lines = None;
-		while let Some(result) = stream.next().await {
-			// Get the size of the tty.
-			let size = ct::terminal::size().map_or((64, 64), |(columns, rows)| {
-				(columns.to_usize().unwrap(), rows.to_usize().unwrap())
-			});
+		let mut state = State {
+			indicators: IndexMap::new(),
+			lines: None,
+			tty,
+		};
 
-			// Clear the indicators.
-			if let Some(lines) = lines {
+		let timeout = std::time::Duration::from_millis(20);
+		loop {
+			let future = tokio::time::timeout(timeout, stream.next());
+			match future.await {
+				Ok(Some(Ok(tg::progress::Event::Output(output)))) => {
+					return Ok(output);
+				},
+				Ok(Some(Err(error))) => {
+					return Err(error);
+				},
+				Ok(None) => {
+					return Err(tg::error!("stream ended without output"));
+				},
+				Ok(Some(Ok(event))) => {
+					state.handle_event(event);
+				},
+				Err(_) => (),
+			}
+			state.print();
+		}
+	}
+}
+
+impl State {
+	fn handle_event<T>(&mut self, event: tg::progress::Event<T>) {
+		match event {
+			tg::progress::Event::Log(log) => {
+				if let Some(level) = log.level {
+					match level {
+						tg::progress::Level::Success => {
+							write!(self.tty, "{} ", "success".green().bold()).unwrap();
+						},
+						tg::progress::Level::Info => {
+							write!(self.tty, "{} ", "info".blue().bold()).unwrap();
+						},
+						tg::progress::Level::Warning => {
+							write!(self.tty, "{} ", "warning".yellow().bold()).unwrap();
+						},
+						tg::progress::Level::Error => {
+							write!(self.tty, "{} ", "error".red().bold()).unwrap();
+						},
+					}
+				}
+				writeln!(self.tty, "{}", log.message).unwrap();
+			},
+
+			tg::progress::Event::Diagnostic(diagnostic) => {
+				Cli::print_diagnostic(&diagnostic);
+			},
+
+			tg::progress::Event::Start(indicator) | tg::progress::Event::Update(indicator) => {
+				self.indicators.insert(indicator.name.clone(), indicator);
+			},
+
+			tg::progress::Event::Finish(indicator) => {
+				self.indicators.shift_remove(&indicator.name);
+			},
+
+			tg::progress::Event::Output(_) => unreachable!(),
+		}
+	}
+
+	fn print(&mut self) {
+		// Get the size of the tty.
+		let size = ct::terminal::size().map_or((64, 64), |(columns, rows)| {
+			(columns.to_usize().unwrap(), rows.to_usize().unwrap())
+		});
+
+		// Move the cursor back.
+		match self.lines {
+			Some(n) if n > 0 => {
 				ct::queue!(
-					tty,
-					ct::cursor::MoveToPreviousLine(lines),
-					ct::terminal::Clear(ct::terminal::ClearType::FromCursorDown),
+					self.tty,
+					ct::cursor::MoveToPreviousLine(self.indicators.len().to_u16().unwrap()),
 				)
 				.unwrap();
-			}
-
-			let event = match result {
-				Ok(event) => event,
-				Err(error) => return Err(error),
-			};
-
-			match event {
-				tg::progress::Event::Log(log) => {
-					if let Some(level) = log.level {
-						match level {
-							tg::progress::Level::Success => {
-								write!(tty, "{} ", "success".green().bold()).unwrap();
-							},
-							tg::progress::Level::Info => {
-								write!(tty, "{} ", "info".blue().bold()).unwrap();
-							},
-							tg::progress::Level::Warning => {
-								write!(tty, "{} ", "warning".yellow().bold()).unwrap();
-							},
-							tg::progress::Level::Error => {
-								write!(tty, "{} ", "error".red().bold()).unwrap();
-							},
-						}
-					}
-					writeln!(tty, "{}", log.message).unwrap();
-				},
-
-				tg::progress::Event::Diagnostic(diagnostic) => {
-					Self::print_diagnostic(&diagnostic);
-				},
-
-				tg::progress::Event::Start(indicator) | tg::progress::Event::Update(indicator) => {
-					indicators.insert(indicator.name.clone(), indicator);
-				},
-
-				tg::progress::Event::Finish(indicator) => {
-					indicators.shift_remove(&indicator.name);
-				},
-
-				tg::progress::Event::Output(value) => {
-					return Ok(value);
-				},
-			}
-
-			// Render the indicators.
-			let title_length = indicators
-				.values()
-				.map(|indicator| indicator.title.len())
-				.max();
-			let now = std::time::SystemTime::now()
-				.duration_since(std::time::UNIX_EPOCH)
-				.unwrap()
-				.as_millis();
-			for indicator in indicators.values() {
-				const LENGTH: u64 = 20;
-				const SPINNER: [char; 10] = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
-				let position = (now / (1000 / 10)) % 10;
-				let position = position.to_usize().unwrap();
-				let spinner = crossterm::style::Stylize::blue(SPINNER[position]);
-				let mut line = String::new();
-				write!(
-					tty,
-					"{:title_length$}",
-					indicator.title,
-					title_length = title_length.unwrap(),
-				)
-				.unwrap();
-				write!(line, " {spinner}").unwrap();
-				if let (Some(current), Some(total)) = (indicator.current, indicator.total) {
-					let current = current.min(total);
-					write!(line, " [").unwrap();
-					let last = current * LENGTH / total;
-					for _ in 0..last {
-						write!(line, "=").unwrap();
-					}
-					if current < total {
-						write!(line, ">").unwrap();
-					} else {
-						write!(line, "=").unwrap();
-					}
-					for _ in last..LENGTH {
-						write!(line, " ").unwrap();
-					}
-					write!(line, "]").unwrap();
-				}
-				if let Some(current) = indicator.current {
-					match indicator.format {
-						tg::progress::IndicatorFormat::Normal => {
-							write!(line, " {current}").unwrap();
-						},
-						tg::progress::IndicatorFormat::Bytes => {
-							let current = byte_unit::Byte::from_u64(current)
-								.get_appropriate_unit(byte_unit::UnitType::Decimal);
-							write!(line, " {current:#.1}").unwrap();
-						},
-					}
-					if let Some(total) = indicator.total {
-						match indicator.format {
-							tg::progress::IndicatorFormat::Normal => {
-								write!(line, " of {total}").unwrap();
-							},
-							tg::progress::IndicatorFormat::Bytes => {
-								let total = byte_unit::Byte::from_u64(total)
-									.get_appropriate_unit(byte_unit::UnitType::Decimal);
-								write!(line, " of {total:#.1}").unwrap();
-							},
-						}
-						let percent = 100.0 * current.to_f64().unwrap() / total.to_f64().unwrap();
-						write!(line, " {percent:.2}%").unwrap();
-					}
-				}
-				let line = clip(&line, size.0);
-				writeln!(tty, "{line}").unwrap();
-			}
-			lines = Some(indicators.len().to_u16().unwrap());
-
-			// Flush the tty.
-			tty.flush().unwrap();
+			},
+			_ => (),
 		}
 
-		Err(tg::error!("stream ended without output"))
+		// Clear the indicators.
+		ct::queue!(
+			self.tty,
+			ct::terminal::Clear(ct::terminal::ClearType::FromCursorDown),
+		)
+		.unwrap();
+
+		// Render the indicators.
+		let title_length = self
+			.indicators
+			.values()
+			.map(|indicator| indicator.title.len())
+			.max();
+		let now = std::time::SystemTime::now()
+			.duration_since(std::time::UNIX_EPOCH)
+			.unwrap()
+			.as_millis();
+		for indicator in self.indicators.values() {
+			const LENGTH: u64 = 20;
+			const SPINNER: [char; 10] = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
+			let position = (now / (1000 / 10)) % 10;
+			let position = position.to_usize().unwrap();
+			let spinner = crossterm::style::Stylize::blue(SPINNER[position]);
+			let mut line = String::new();
+			write!(
+				self.tty,
+				"{:title_length$}",
+				indicator.title,
+				title_length = title_length.unwrap(),
+			)
+			.unwrap();
+			write!(line, " {spinner}").unwrap();
+			if let (Some(current), Some(total)) = (indicator.current, indicator.total) {
+				let current = current.min(total);
+				write!(line, " [").unwrap();
+				let last = current * LENGTH / total;
+				for _ in 0..last {
+					write!(line, "=").unwrap();
+				}
+				if current < total {
+					write!(line, ">").unwrap();
+				} else {
+					write!(line, "=").unwrap();
+				}
+				for _ in last..LENGTH {
+					write!(line, " ").unwrap();
+				}
+				write!(line, "]").unwrap();
+			}
+			if let Some(current) = indicator.current {
+				match indicator.format {
+					tg::progress::IndicatorFormat::Normal => {
+						write!(line, " {current}").unwrap();
+					},
+					tg::progress::IndicatorFormat::Bytes => {
+						let current = byte_unit::Byte::from_u64(current)
+							.get_appropriate_unit(byte_unit::UnitType::Decimal);
+						write!(line, " {current:#.1}").unwrap();
+					},
+				}
+				if let Some(total) = indicator.total {
+					match indicator.format {
+						tg::progress::IndicatorFormat::Normal => {
+							write!(line, " of {total}").unwrap();
+						},
+						tg::progress::IndicatorFormat::Bytes => {
+							let total = byte_unit::Byte::from_u64(total)
+								.get_appropriate_unit(byte_unit::UnitType::Decimal);
+							write!(line, " of {total:#.1}").unwrap();
+						},
+					}
+					let percent = 100.0 * current.to_f64().unwrap() / total.to_f64().unwrap();
+					write!(line, " {percent:.2}%").unwrap();
+				}
+			}
+			let line = clip(&line, size.0);
+			writeln!(self.tty, "{line}").unwrap();
+		}
+		self.lines.replace(self.indicators.len().to_u16().unwrap());
+
+		// Flush the tty.
+		self.tty.flush().unwrap();
 	}
 }
