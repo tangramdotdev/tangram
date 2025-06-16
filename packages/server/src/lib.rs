b8746638fbc6541a09473e23d248c92f474b1b39
Mike Hilgendorf <mike@hilgendorf.audio>
2025-06-16 11:42:29 -0500

    feat: add .tangram/tags directory and update `tg tag` logic
    - internally forbid the creation of "branch" tags
    - make `tg tag list foo` equivalent to `tg tag list foo/*` when `foo` is a "branch"
    - create a .tangram/tags directory for containing local tags
    - make LSP URIs point into the tags directory for more legible/discoverable resolutions

--- packages/server/src/lib.rs
+++ packages/server/src/lib.rs
@@ -891,6 +891,11 @@ impl Server {
 	}
 
 	#[must_use]
+	pub fn tags_path(&self) -> PathBuf {
+		self.path.join("tags")
+	}
+
+	#[must_use]
 	pub fn temp_path(&self) -> PathBuf {
 		self.path.join("tmp")
 	}
