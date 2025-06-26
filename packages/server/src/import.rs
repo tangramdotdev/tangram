9b92db97b0f44f73e16bcf491ddca1d263113efd
David Yamnitsky <david@yamnitsky.com>
2025-06-26 14:26:13 -0400

    fix(server/import): limit concurrent tasks for complete sending

--- packages/server/src/import.rs
+++ packages/server/src/import.rs
@@ -1,7 +1,6 @@
 use crate::{Server, store::Store};
-use bytes::Bytes;
 use futures::{
-	FutureExt as _, Stream, StreamExt as _, TryFutureExt as _, TryStreamExt as _, future,
+	FutureExt as _, Stream, StreamExt, TryFutureExt as _, TryStreamExt as _, future,
 	stream::{self, FuturesUnordered},
 };
 use indoc::formatdoc;
@@ -17,9 +16,10 @@ use tangram_either::Either;
 use tangram_futures::{stream::Ext as _, task::Stop};
 use tangram_http::{Body, request::Ext as _};
 use tangram_messenger::prelude::*;
-use tokio::task::JoinSet;
 use tokio_stream::wrappers::{IntervalStream, ReceiverStream};
 use tokio_util::task::AbortOnDropHandle;
+
+const COMPLETE_CONCURRENCY: usize = 8;
 
 #[derive(Debug)]
 struct Progress {
@@ -201,61 +201,70 @@ impl Server {
 		event_sender: &tokio::sync::mpsc::Sender<tg::Result<tg::import::Event>>,
 	) -> tg::Result<()> {
 		let stream = ReceiverStream::new(complete_receiver);
-		let mut stream = pin!(stream);
-		let mut join_set = JoinSet::new();
-		while let Some(item) = stream.next().await {
-			let server = self.clone();
-			let event_sender = event_sender.clone();
-			join_set.spawn(async move {
-				match item {
-					Either::Left(id) => {
-						let result = server.try_get_import_process_complete(&id).await;
-						let process_complete = match result {
-							Ok(process_complete) => process_complete,
-							Err(error) => {
-								tracing::error!(?error, "failed to get process complete");
-								return;
-							},
-						};
-						let Some(process_complete) = process_complete else {
-							return;
-						};
-						if !(process_complete.complete
-							|| process_complete.commands_complete
-							|| process_complete.outputs_complete)
-						{
-							return;
-						}
-						let event = tg::import::Event::Complete(tg::import::Complete::Process(
-							process_complete,
-						));
-						event_sender.send(Ok(event)).await.ok();
-					},
-					Either::Right(id) => {
-						let result = server.try_get_import_object_complete(&id).await;
-						let object_complete = match result {
-							Ok(object_complete) => object_complete,
-							Err(error) => {
-								tracing::error!(?error, "failed to get object complete");
-								return;
-							},
-						};
-						let Some(object_complete) = object_complete else {
-							return;
-						};
-						if !object_complete {
-							return;
-						}
-						let event = tg::import::Event::Complete(tg::import::Complete::Object(
-							tg::import::ObjectComplete { id },
-						));
-						event_sender.send(Ok(event)).await.ok();
-					},
+		let stream = pin!(stream);
+		stream
+			.for_each_concurrent(COMPLETE_CONCURRENCY, {
+				let server = self.clone();
+				let event_sender = event_sender.clone();
+				move |item| {
+					let server = server.clone();
+					let event_sender = event_sender.clone();
+					async move { server.import_complete_task_item(item, &event_sender).await }
 				}
-			});
-		}
-		join_set.join_all().await;
+			})
+			.await;
 		Ok(())
+	}
+
+	async fn import_complete_task_item(
+		&self,
+		item: Either<tg::process::Id, tg::object::Id>,
+		event_sender: &tokio::sync::mpsc::Sender<tg::Result<tg::import::Event>>,
+	) {
+		match item {
+			Either::Left(id) => {
+				let result = self.try_get_import_process_complete(&id).await;
+				let process_complete = match result {
+					Ok(process_complete) => process_complete,
+					Err(error) => {
+						tracing::error!(?error, "failed to get process complete");
+						return;
+					},
+				};
+				let Some(process_complete) = process_complete else {
+					return;
+				};
+				if !(process_complete.complete
+					|| process_complete.commands_complete
+					|| process_complete.outputs_complete)
+				{
+					return;
+				}
+				let event =
+					tg::import::Event::Complete(tg::import::Complete::Process(process_complete));
+				event_sender.send(Ok(event)).await.ok();
+			},
+			Either::Right(id) => {
+				let result = self.try_get_import_object_complete(&id).await;
+				let object_complete = match result {
+					Ok(object_complete) => object_complete,
+					Err(error) => {
+						tracing::error!(?error, "failed to get object complete");
+						return;
+					},
+				};
+				let Some(object_complete) = object_complete else {
+					return;
+				};
+				if !object_complete {
+					return;
+				}
+				let event = tg::import::Event::Complete(tg::import::Complete::Object(
+					tg::import::ObjectComplete { id },
+				));
+				event_sender.send(Ok(event)).await.ok();
+			},
+		}
 	}
 
 	async fn try_get_import_process_complete(
@@ -365,135 +374,134 @@ impl Server {
 		event_sender: &tokio::sync::mpsc::Sender<tg::Result<tg::import::Event>>,
 		progress: &Arc<Progress>,
 	) -> tg::Result<()> {
-		// Choose the parameters.
-		let (n_batch_tasks, max_objects_per_batch, max_bytes_per_batch) = match &self.store {
+		// Choose the batch parameters.
+		let (concurrency, max_objects_per_batch, max_bytes_per_batch) = match &self.store {
 			#[cfg(feature = "foundationdb")]
-			Store::Fdb(_) => (10, 1_000, 5_000_000),
+			Store::Fdb(_) => (8, 1_000, 5_000_000),
 			Store::Lmdb(_) => (1, 1_000, 5_000_000),
 			Store::Memory(_) => (1, 1, u64::MAX),
 			Store::S3(_) => (256, 1, u64::MAX),
 		};
 
-		// Spawn tasks that receive batches and write them to the store and messenger.
-		let (batch_sender, batch_receiver) =
-			async_channel::bounded::<Vec<(tg::object::Id, Bytes)>>(1);
-		let mut tasks = JoinSet::new();
-		for _ in 0..n_batch_tasks {
-			tasks.spawn({
-				let server = self.clone();
-				let batch_receiver = batch_receiver.clone();
-				let event_sender = event_sender.clone();
-				let progress = progress.clone();
-				async move {
-					server
-						.import_objects_task_batch_task(batch_receiver, &event_sender, &progress)
-						.await;
+		// Create a stream of batches.
+		struct State {
+			object_receiver: tokio::sync::mpsc::Receiver<tg::export::ObjectItem>,
+			item: Option<tg::export::ObjectItem>,
+		}
+		let state = State {
+			item: None,
+			object_receiver,
+		};
+		let stream = stream::unfold(state, |mut state| async {
+			let mut batch_bytes = state
+				.item
+				.as_ref()
+				.map(|item| item.bytes.len().to_u64().unwrap())
+				.unwrap_or_default();
+			let mut batch = state.item.take().map(|item| vec![item]).unwrap_or_default();
+			while let Some(item) = state.object_receiver.recv().await {
+				let size = item.bytes.len().to_u64().unwrap();
+				if !batch.is_empty()
+					&& (batch.len() + 1 >= max_objects_per_batch
+						|| batch_bytes + size >= max_bytes_per_batch)
+				{
+					state.item.replace(item);
+					return Some((batch, state));
 				}
-			});
-		}
-
-		// Receive the objects in batches and send them to the tasks.
-		let mut batch_bytes = 0;
-		let mut batch = Vec::new();
-		let stream = ReceiverStream::new(object_receiver);
-		let mut stream = pin!(stream);
-		while let Some(tg::export::ObjectItem { id, bytes }) = stream.next().await {
-			if !batch.is_empty()
-				&& (batch.len() > max_objects_per_batch || batch_bytes > max_bytes_per_batch)
-			{
-				batch_bytes = 0;
-				batch_sender.send(std::mem::take(&mut batch)).await.unwrap();
+				batch_bytes += size;
+				batch.push(item);
 			}
-			batch_bytes += bytes.len().to_u64().unwrap();
-			batch.push((id, bytes));
-		}
-		batch_sender.send(batch).await.unwrap();
+			if batch.is_empty() {
+				return None;
+			}
+			Some((batch, state))
+		});
 
-		// Drop the batch sender.
-		drop(batch_sender);
-
-		// Join the tasks.
-		tasks.join_all().await;
+		// Write the batches.
+		stream
+			.for_each_concurrent(concurrency, |batch| {
+				self.import_objects_task_batch(batch, event_sender, progress)
+			})
+			.await;
 
 		Ok(())
 	}
 
-	async fn import_objects_task_batch_task(
+	async fn import_objects_task_batch(
 		&self,
-		batch_receiver: async_channel::Receiver<Vec<(tg::object::Id, Bytes)>>,
+		batch: Vec<tg::export::ObjectItem>,
 		event_sender: &tokio::sync::mpsc::Sender<tg::Result<tg::import::Event>>,
 		progress: &Arc<Progress>,
 	) {
-		while let Ok(batch) = batch_receiver.recv().await {
-			let touched_at = time::OffsetDateTime::now_utc().unix_timestamp();
+		let touched_at = time::OffsetDateTime::now_utc().unix_timestamp();
 
-			// Create the store future.
-			let store_future = {
-				async {
-					let batch = batch
-						.clone()
-						.into_iter()
-						.map(|(id, bytes)| (id, Some(bytes), None))
-						.collect();
-					let arg = crate::store::PutBatchArg {
-						objects: batch,
-						touched_at,
-					};
-					self.store.put_batch(arg).await?;
-					Ok::<_, tg::Error>(())
-				}
-			};
-
-			// Create the messenger future.
-			let messenger_future = {
-				async {
-					batch
-						.iter()
-						.map(|(id, bytes)| async {
-							let data = tg::object::Data::deserialize(id.kind(), bytes.clone())?;
-							let message =
-								crate::index::Message::PutObject(crate::index::PutObjectMessage {
-									cache_reference: None,
-									children: data.children().collect(),
-									id: id.clone(),
-									size: bytes.len().to_u64().unwrap(),
-									touched_at,
-								});
-							let message = serde_json::to_vec(&message).map_err(|source| {
-								tg::error!(!source, "failed to serialize the message")
-							})?;
-							let _published = self
-								.messenger
-								.stream_publish("index".to_owned(), message.into())
-								.await
-								.map_err(|source| {
-									tg::error!(!source, "failed to publish the message")
-								})?;
-							Ok::<_, tg::Error>(())
-						})
-						.collect::<FuturesUnordered<_>>()
-						.try_collect::<()>()
-						.await?;
-					Ok::<_, tg::Error>(())
-				}
-			};
-
-			// Join the store and messenger futures.
-			let result = futures::try_join!(store_future, messenger_future);
-			if let Err(error) = result {
-				let error = tg::error!(!error, "failed to join the futures");
-				event_sender.send(Err(error)).await.ok();
+		// Create the store future.
+		let store_future = {
+			async {
+				let batch = batch
+					.clone()
+					.into_iter()
+					.map(|item| (item.id, Some(item.bytes), None))
+					.collect();
+				let arg = crate::store::PutBatchArg {
+					objects: batch,
+					touched_at,
+				};
+				self.store.put_batch(arg).await?;
+				Ok::<_, tg::Error>(())
 			}
+		};
 
-			// Update the progress.
-			let objects = batch.len().to_u64().unwrap();
-			let bytes = batch
-				.iter()
-				.map(|(_, bytes)| bytes.len().to_u64().unwrap())
-				.sum();
-			progress.increment_objects(objects);
-			progress.increment_bytes(bytes);
+		// Create the messenger future.
+		let messenger_future = {
+			async {
+				batch
+					.iter()
+					.map(|item| async {
+						let data =
+							tg::object::Data::deserialize(item.id.kind(), item.bytes.clone())?;
+						let message =
+							crate::index::Message::PutObject(crate::index::PutObjectMessage {
+								cache_reference: None,
+								children: data.children().collect(),
+								id: item.id.clone(),
+								size: item.bytes.len().to_u64().unwrap(),
+								touched_at,
+							});
+						let message = serde_json::to_vec(&message).map_err(|source| {
+							tg::error!(!source, "failed to serialize the message")
+						})?;
+						let _published = self
+							.messenger
+							.stream_publish("index".to_owned(), message.into())
+							.await
+							.map_err(|source| {
+								tg::error!(!source, "failed to publish the message")
+							})?;
+						Ok::<_, tg::Error>(())
+					})
+					.collect::<FuturesUnordered<_>>()
+					.try_collect::<()>()
+					.await?;
+				Ok::<_, tg::Error>(())
+			}
+		};
+
+		// Join the store and messenger futures.
+		let result = futures::try_join!(store_future, messenger_future);
+		if let Err(error) = result {
+			let error = tg::error!(!error, "failed to join the futures");
+			event_sender.send(Err(error)).await.ok();
 		}
+
+		// Update the progress.
+		let objects = batch.len().to_u64().unwrap();
+		let bytes = batch
+			.iter()
+			.map(|item| item.bytes.len().to_u64().unwrap())
+			.sum();
+		progress.increment_objects(objects);
+		progress.increment_bytes(bytes);
 	}
 
 	pub(crate) async fn handle_import_request<H>(
