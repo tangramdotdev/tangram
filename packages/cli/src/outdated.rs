e99dfa2036da339230ad31d069f7f2ee92516504
Mike Hilgendorf <mike@hilgendorf.audio>
2025-10-17 11:56:36 -0500

    feat(cli): implement tg outdated
    - implement `tg outdated` command
    - add `tg::object::visit` helper for walking object graphs

--- packages/cli/src/outdated.rs
+++ packages/cli/src/outdated.rs
@@ -1,4 +1,4 @@
-use {crate::Cli, tangram_client as tg};
+use {crate::Cli, std::collections::HashSet, tangram_client as tg};
 
 /// Get a package's outdated dependencies.
 #[derive(Clone, Debug, clap::Args)]
@@ -9,7 +9,97 @@ pub struct Args {
 }
 
 impl Cli {
-	pub async fn command_outdated(&mut self, _args: Args) -> tg::Result<()> {
-		Err(tg::error!("unimplemented"))
+	pub async fn command_outdated(&mut self, args: Args) -> tg::Result<()> {
+		let handle = self.handle().await?;
+		let object = self
+			.get_reference(&args.reference)
+			.await
+			.map_err(|source| tg::error!(!source, "failed to get reference"))?
+			.item()
+			.clone()
+			.right()
+			.ok_or_else(|| tg::error!(%reference = args.reference, "expected an object"))?;
+		let mut visitor = Visitor::default();
+		tg::object::visit(&handle, &mut visitor, [object])
+			.await
+			.map_err(|source| tg::error!(!source, "failed to walk objects"))?;
+		let entries = visitor.entries.into_iter().collect::<Vec<_>>();
+		let output = serde_json::to_string_pretty(&entries)
+			.map_err(|source| tg::error!(!source, "failed to serialize entries"))?;
+		println!("{output}");
+		Ok(())
+	}
+}
+
+#[derive(Default)]
+struct Visitor {
+	entries: HashSet<Entry>,
+}
+
+#[derive(serde::Serialize, Debug, PartialEq, Eq, Hash)]
+struct Entry {
+	compatible: Option<tg::Tag>,
+	current: tg::Tag,
+	latest: Option<tg::Tag>,
+}
+
+impl<H> tg::object::Visitor<H> for Visitor
+where
+	H: tg::Handle,
+{
+	async fn visit_file(
+		&mut self,
+		handle: &H,
+		file: &tangram_client::File,
+	) -> tangram_client::Result<()> {
+		// Get the file's dependencies.
+		let dependencies = file
+			.dependencies(handle)
+			.await
+			.map_err(|source| tg::error!(!source, "failed to get the file's dependencies"))?
+			.into_iter()
+			.filter_map(|(reference, referent)| {
+				let pattern = reference.item().clone().try_unwrap_tag().ok()?;
+				let tag = referent?.options.tag.take()?;
+				Some((pattern, tag))
+			});
+		for (pattern, current) in dependencies {
+			let compatible = handle
+				.list_tags(tg::tag::list::Arg {
+					pattern: pattern.clone(),
+					length: Some(1),
+					remote: None,
+					reverse: false,
+				})
+				.await?
+				.data
+				.into_iter()
+				.map(|o| o.tag)
+				.next();
+
+			let mut components = pattern.components().collect::<Vec<_>>();
+			components.pop();
+			components.push("*");
+			let pattern = tg::tag::Pattern::new(components.join("/"));
+			let latest = handle
+				.list_tags(tg::tag::list::Arg {
+					pattern: pattern.clone(),
+					length: Some(1),
+					remote: None,
+					reverse: true,
+				})
+				.await?
+				.data
+				.into_iter()
+				.map(|o| o.tag)
+				.next();
+			let entry = Entry {
+				current,
+				compatible,
+				latest,
+			};
+			self.entries.insert(entry);
+		}
+		Ok(())
 	}
 }
--- packages/client/src/object.rs
+++ packages/client/src/object.rs
@@ -2,6 +2,9 @@ pub use self::{
 	data::Object as Data, handle::Object as Handle, id::Id, kind::Kind, metadata::Metadata,
 	object::Object, state::State,
 };
+use crate as tg;
+use futures::future;
+use std::{collections::HashSet, future::Future};
 
 pub mod data;
 pub mod get;
@@ -14,3 +17,113 @@ pub mod object;
 pub mod put;
 pub mod state;
 pub mod touch;
+
+/// Visit each object in an object graph once. Note: this is _not_ the same as walking the object tree, since it will also construct objects that point into graph objects to visit each of them.
+pub async fn visit<H, V>(
+	handle: &H,
+	visitor: &mut V,
+	objects: impl IntoIterator<Item = tg::Object>,
+) -> tg::Result<()>
+where
+	H: tg::Handle,
+	V: tg::object::Visitor<H>,
+{
+	// Stack for DFS.
+	let mut stack = objects.into_iter().collect::<Vec<_>>();
+
+	// Keep track of objects we've visited already.
+	let mut visited = HashSet::<_, fnv::FnvBuildHasher>::default();
+
+	// Recursive DFS.
+	while let Some(object) = stack.pop() {
+		// Don't visit objects more than once.
+		if !visited.insert(object.id()) {
+			continue;
+		}
+
+		// Apply the visitor method.
+		match &object {
+			tg::Object::Blob(blob) => visitor.visit_blob(handle, blob).await?,
+			tg::Object::Directory(directory) => visitor.visit_directory(handle, directory).await?,
+			tg::Object::File(file) => visitor.visit_file(handle, file).await?,
+			tg::Object::Symlink(symlink) => visitor.visit_symlink(handle, symlink).await?,
+			tg::Object::Graph(graph) => visitor.visit_graph(handle, graph).await?,
+			tg::Object::Command(command) => visitor.visit_command(handle, command).await?,
+		}
+
+		// For graphs we want to visit the objects that point into it as well. The visited set will make sure we don't visit the same graph twice.
+		if let tg::Object::Graph(graph) = &object {
+			let children =
+				graph
+					.nodes(handle)
+					.await?
+					.into_iter()
+					.enumerate()
+					.map(|(index, node)| match node.kind() {
+						tg::artifact::Kind::Directory => {
+							tg::Directory::with_graph_and_node(graph.clone(), index).into()
+						},
+						tg::artifact::Kind::File => {
+							tg::File::with_graph_and_node(graph.clone(), index).into()
+						},
+						tg::artifact::Kind::Symlink => {
+							tg::Symlink::with_graph_and_node(graph.clone(), index).into()
+						},
+					});
+			stack.extend(children);
+		} else {
+			// For everything else just visit the children.
+			stack.extend(object.children(handle).await?.into_iter());
+		}
+
+		// Unload the object since we probably don't need it anymore.
+		object.unload();
+	}
+	Ok(())
+}
+
+#[allow(unused_variables)]
+pub trait Visitor<H>
+where
+	H: tg::Handle,
+{
+	fn visit_blob(&mut self, handle: &H, blob: &tg::Blob) -> impl Future<Output = tg::Result<()>> {
+		future::ok(())
+	}
+
+	fn visit_directory(
+		&mut self,
+		handle: &H,
+		directory: &tg::Directory,
+	) -> impl Future<Output = tg::Result<()>> {
+		future::ok(())
+	}
+
+	fn visit_file(&mut self, handle: &H, file: &tg::File) -> impl Future<Output = tg::Result<()>> {
+		future::ok(())
+	}
+
+	fn visit_symlink(
+		&mut self,
+		handle: &H,
+		symlink: &tg::Symlink,
+	) -> impl Future<Output = tg::Result<()>> {
+		future::ok(())
+	}
+
+	fn visit_graph(
+		&mut self,
+		handle: &H,
+		graph: &tg::Graph,
+	) -> impl Future<Output = tg::Result<()>> {
+		future::ok(())
+	}
+
+	fn visit_command(
+		&mut self,
+		handle: &H,
+		command: &tg::Command,
+	) -> impl Future<Output = tg::Result<()>> {
+		future::ok(())
+	}
+}
