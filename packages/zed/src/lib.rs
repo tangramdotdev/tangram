d224b3bdff5b063bec3734a71b9fbc79e4282117
David Yamnitsky <david@yamnitsky.com>
2025-10-22 10:39:15 -0400

    feat: add zed extension

---
+++ packages/zed/src/lib.rs
@@ -0,0 +1,27 @@
+use zed_extension_api::{self as zed, Result};
+
+struct Extension;
+
+impl zed::Extension for Extension {
+	fn new() -> Self {
+		Self
+	}
+
+	fn language_server_command(
+		&mut self,
+		_language_server_id: &zed::LanguageServerId,
+		worktree: &zed::Worktree,
+	) -> Result<zed::Command> {
+		let tg = worktree
+			.which("tangram")
+			.ok_or("failed to find the tangram binary")?;
+		let command = zed::Command {
+			command: tg,
+			args: vec!["lsp".to_string()],
+			env: vec![],
+		};
+		Ok(command)
+	}
+}
+
+zed::register_extension!(Extension);
