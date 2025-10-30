083b5f8a6f5266c139ae183afe02a78ea5bb7d02
David Yamnitsky <david@yamnitsky.com>
2025-10-30 19:34:42 -0400

    feat: temporarily restore use of biome for formatting

--- packages/module/src/format.rs
+++ packages/module/src/format.rs
@@ -6,7 +6,45 @@ pub struct Output {
 }
 
 #[must_use]
-pub fn format(module: &tg::module::Data, text: &str) -> Output {
+pub fn format(_module: &tg::module::Data, text: &str) -> Output {
+	let source_type = biome_js_syntax::JsFileSource::ts();
+	let options = biome_js_parser::JsParserOptions::default();
+	let node = biome_js_parser::parse(text, source_type, options);
+	let options = biome_js_formatter::context::JsFormatOptions::new(source_type);
+	let formatted = match biome_js_formatter::format_node(options, &node.syntax()) {
+		Ok(formatted) => formatted,
+		Err(error) => {
+			let diagnostics = vec![tg::diagnostic::Data {
+				location: None,
+				severity: tg::diagnostic::Severity::Error,
+				message: error.to_string(),
+			}];
+			return Output {
+				diagnostics,
+				text: text.to_owned(),
+			};
+		},
+	};
+	let text = match formatted.print() {
+		Ok(printed) => printed.into_code(),
+		Err(error) => {
+			let diagnostics = vec![tg::diagnostic::Data {
+				location: None,
+				severity: tg::diagnostic::Severity::Error,
+				message: error.to_string(),
+			}];
+			return Output {
+				diagnostics,
+				text: text.to_owned(),
+			};
+		},
+	};
+	let diagnostics = Vec::new();
+	Output { diagnostics, text }
+}
+
+#[must_use]
+pub fn format_oxc(module: &tg::module::Data, text: &str) -> Output {
 	let allocator = oxc::allocator::Allocator::default();
 
 	// Create the formatter.
