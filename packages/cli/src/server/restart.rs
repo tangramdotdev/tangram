31521504f25cceed5cab22a2b542d39fef489232
Mike Hilgendorf <mike@hilgendorf.audio>
2025-11-04 10:10:58 -0600

    fix(cli): issue warning for `tg server restart` if stop() fails (#665)

--- packages/cli/src/server/restart.rs
+++ packages/cli/src/server/restart.rs
@@ -7,7 +7,9 @@ pub struct Args {}
 
 impl Cli {
 	pub async fn command_server_restart(&mut self, _args: Args) -> tg::Result<()> {
-		self.stop_server().await?;
+		if let Err(error) = self.stop_server().await {
+			Self::print_warning_message(&format!("failed to stop existing server. {error}"));
+		}
 		self.start_server().await?;
 		Ok(())
 	}
