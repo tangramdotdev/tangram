0dffd7cea941cc697cb870f3381fcb7362750aa1
Mike Hilgendorf <mike@hilgendorf.audio>
2025-11-04 09:44:28 -0600

    use internal formatter

--- packages/cli/src/server/restart.rs
+++ packages/cli/src/server/restart.rs
@@ -8,7 +8,7 @@ pub struct Args {}
 impl Cli {
 	pub async fn command_server_restart(&mut self, _args: Args) -> tg::Result<()> {
 		if let Err(error) = self.stop_server().await {
-			eprintln!("{}: failed to stop existing server. {error}", "warning".yellow());
+			Self::print_warning_message(&format!("failed to stop existing server. {error}"));
 		}
 		self.start_server().await?;
 		Ok(())
