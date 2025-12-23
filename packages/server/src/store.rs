1fbeaac7e4b6685007d95f26fe0f28c0ec7d1103
David Yamnitsky <david@yamnitsky.com>
2025-12-23 09:52:38 -0800

    fix(store): compile error

--- packages/server/src/store.rs
+++ packages/server/src/store.rs
@@ -42,13 +42,13 @@ impl Store {
 			keyspace: config.keyspace.clone(),
 			password: config.password.clone(),
 			speculative_execution: config.speculative_execution.as_ref().map(|se| match se {
-				crate::config::ScyllaSpeculativeExecution::Percentile(p) => {
+				crate::config::ScyllaStoreSpeculativeExecution::Percentile(p) => {
 					store::scylla::SpeculativeExecution::Percentile {
 						max_retry_count: p.max_retry_count,
 						percentile: p.percentile,
 					}
 				},
-				crate::config::ScyllaSpeculativeExecution::Simple(s) => {
+				crate::config::ScyllaStoreSpeculativeExecution::Simple(s) => {
 					store::scylla::SpeculativeExecution::Simple {
 						max_retry_count: s.max_retry_count,
 						retry_interval: std::time::Duration::from_millis(s.retry_interval),
