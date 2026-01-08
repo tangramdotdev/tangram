456af68def52d15d1dbc9d4100d88475c14c0780
David Yamnitsky <david@yamnitsky.com>
2026-01-08 16:58:04 -0500

    refactor(futures/task/map): support weak tasks

--- packages/futures/src/task/map.rs
+++ packages/futures/src/task/map.rs
@@ -10,9 +10,7 @@ use {
 	},
 };
 
-pub struct Map<K, T, C = (), H = RandomState> {
-	map: Arc<DashMap<K, Shared<T, C>, H>>,
-}
+pub struct Map<K, T, C = (), H = RandomState>(Arc<DashMap<K, Shared<T, C>, H>>);
 
 impl<K, T, C, H> Map<K, T, C, H>
 where
@@ -27,7 +25,16 @@ where
 		F: FnOnce(C, Stop) -> Fut,
 		Fut: Future<Output = T> + Send + 'static,
 	{
-		self.spawn_inner(key, context, f, false)
+		self.spawn_inner(key, context, f, false, false)
+	}
+
+	pub fn spawn_detached_with_context<G, F, Fut>(&self, key: K, context: G, f: F) -> Shared<T, C>
+	where
+		G: FnOnce() -> C,
+		F: FnOnce(C, Stop) -> Fut,
+		Fut: Future<Output = T> + Send + 'static,
+	{
+		self.spawn_inner(key, context, f, false, true)
 	}
 
 	pub fn get_or_spawn_with_context<G, F, Fut>(&self, key: K, context: G, f: F) -> Shared<T, C>
@@ -36,29 +43,53 @@ where
 		F: FnOnce(C, Stop) -> Fut,
 		Fut: Future<Output = T> + Send + 'static,
 	{
-		self.spawn_inner(key, context, f, true)
+		self.spawn_inner(key, context, f, true, false)
 	}
 
-	fn spawn_inner<G, F, Fut>(&self, key: K, context: G, f: F, get: bool) -> Shared<T, C>
+	pub fn get_or_spawn_detached_with_context<G, F, Fut>(
+		&self,
+		key: K,
+		context: G,
+		f: F,
+	) -> Shared<T, C>
 	where
 		G: FnOnce() -> C,
 		F: FnOnce(C, Stop) -> Fut,
 		Fut: Future<Output = T> + Send + 'static,
 	{
-		let map = self.map.clone();
+		self.spawn_inner(key, context, f, true, true)
+	}
+
+	fn spawn_inner<G, F, Fut>(
+		&self,
+		key: K,
+		context: G,
+		f: F,
+		get: bool,
+		detached: bool,
+	) -> Shared<T, C>
+	where
+		G: FnOnce() -> C,
+		F: FnOnce(C, Stop) -> Fut,
+		Fut: Future<Output = T> + Send + 'static,
+	{
+		let map = self.0.clone();
 		let spawn = {
 			let key = key.clone();
+			let weak = Arc::downgrade(&map);
 			move || {
 				let context = context();
 				let mut task = Shared::spawn_with_context(context.clone(), {
 					let key = key.clone();
-					let map = map.clone();
+					let weak = weak.clone();
 					move |stop| {
 						let future = f(context, stop);
 						async move {
 							let result = AssertUnwindSafe(future).catch_unwind().await;
-							let id = tokio::task::id();
-							map.remove_if(&key, |_, task| task.id() == id);
+							if let Some(map) = weak.upgrade() {
+								let id = tokio::task::id();
+								map.remove_if(&key, |_, task| task.id() == id);
+							}
 							match result {
 								Ok(value) => value,
 								Err(payload) => std::panic::resume_unwind(payload),
@@ -70,18 +101,22 @@ where
 					let id = task.id();
 					let key = key.clone();
 					move || {
-						map.remove_if(&key, |_, task| task.id() == id);
+						if let Some(map) = weak.upgrade() {
+							map.remove_if(&key, |_, task| task.id() == id);
+						}
 					}
 				});
-				task.detach();
+				if detached {
+					task.detach();
+				}
 				task
 			}
 		};
 		let mut task = if get {
-			self.map.entry(key).or_insert_with(spawn).value().clone()
+			self.0.entry(key).or_insert_with(spawn).value().clone()
 		} else {
 			let task = spawn();
-			self.map.insert(key, task.clone());
+			self.0.insert(key, task.clone());
 			task
 		};
 		task.attach();
@@ -93,7 +128,20 @@ where
 		G: FnOnce() -> C,
 		F: FnOnce(C, Stop) -> T + Send + 'static,
 	{
-		self.spawn_blocking_inner(key, context, f, false)
+		self.spawn_blocking_inner(key, context, f, false, false)
+	}
+
+	pub fn spawn_blocking_detached_with_context<G, F>(
+		&self,
+		key: K,
+		context: G,
+		f: F,
+	) -> Shared<T, C>
+	where
+		G: FnOnce() -> C,
+		F: FnOnce(C, Stop) -> T + Send + 'static,
+	{
+		self.spawn_blocking_inner(key, context, f, false, true)
 	}
 
 	pub fn get_or_spawn_blocking_with_context<G, F>(&self, key: K, context: G, f: F) -> Shared<T, C>
@@ -101,26 +149,49 @@ where
 		G: FnOnce() -> C,
 		F: FnOnce(C, Stop) -> T + Send + 'static,
 	{
-		self.spawn_blocking_inner(key, context, f, true)
+		self.spawn_blocking_inner(key, context, f, true, false)
 	}
 
-	fn spawn_blocking_inner<G, F>(&self, key: K, context: G, f: F, get: bool) -> Shared<T, C>
+	pub fn get_or_spawn_blocking_detached_with_context<G, F>(
+		&self,
+		key: K,
+		context: G,
+		f: F,
+	) -> Shared<T, C>
 	where
 		G: FnOnce() -> C,
 		F: FnOnce(C, Stop) -> T + Send + 'static,
 	{
-		let map = self.map.clone();
+		self.spawn_blocking_inner(key, context, f, true, true)
+	}
+
+	fn spawn_blocking_inner<G, F>(
+		&self,
+		key: K,
+		context: G,
+		f: F,
+		get: bool,
+		detached: bool,
+	) -> Shared<T, C>
+	where
+		G: FnOnce() -> C,
+		F: FnOnce(C, Stop) -> T + Send + 'static,
+	{
+		let map = self.0.clone();
 		let spawn = {
 			let key = key.clone();
+			let weak = Arc::downgrade(&map);
 			move || {
 				let context = context();
 				let mut task = Shared::spawn_blocking_with_context(context.clone(), {
 					let key = key.clone();
-					let map = map.clone();
+					let weak = weak.clone();
 					move |stop| {
 						let result = catch_unwind(AssertUnwindSafe(|| f(context, stop)));
-						let id = tokio::task::id();
-						map.remove_if(&key, |_, task| task.id() == id);
+						if let Some(map) = weak.upgrade() {
+							let id = tokio::task::id();
+							map.remove_if(&key, |_, task| task.id() == id);
+						}
 						match result {
 							Ok(value) => value,
 							Err(payload) => std::panic::resume_unwind(payload),
@@ -131,18 +202,22 @@ where
 					let id = task.id();
 					let key = key.clone();
 					move || {
-						map.remove_if(&key, |_, task| task.id() == id);
+						if let Some(map) = weak.upgrade() {
+							map.remove_if(&key, |_, task| task.id() == id);
+						}
 					}
 				});
-				task.detach();
+				if detached {
+					task.detach();
+				}
 				task
 			}
 		};
 		let mut task = if get {
-			self.map.entry(key).or_insert_with(spawn).value().clone()
+			self.0.entry(key).or_insert_with(spawn).value().clone()
 		} else {
 			let task = spawn();
-			self.map.insert(key, task.clone());
+			self.0.insert(key, task.clone());
 			task
 		};
 		task.attach();
@@ -150,11 +225,11 @@ where
 	}
 
 	pub fn try_get_id(&self, key: &K) -> Option<tokio::task::Id> {
-		self.map.get(key).map(|task| task.id())
+		self.0.get(key).map(|task| task.id())
 	}
 
 	pub fn try_get(&self, key: &K) -> Option<Shared<T, C>> {
-		self.map.get(key).map(|entry| {
+		self.0.get(key).map(|entry| {
 			let mut task = entry.value().clone();
 			task.attach();
 			task
@@ -162,32 +237,32 @@ where
 	}
 
 	pub fn abort(&self, key: &K) {
-		if let Some(task) = self.map.get(key) {
+		if let Some(task) = self.0.get(key) {
 			task.abort();
 		}
 	}
 
 	pub fn stop(&self, key: &K) {
-		if let Some(task) = self.map.get(key) {
+		if let Some(task) = self.0.get(key) {
 			task.stop();
 		}
 	}
 
 	pub fn abort_all(&self) {
-		for task in self.map.iter() {
+		for task in &*self.0 {
 			task.abort();
 		}
 	}
 
 	pub fn stop_all(&self) {
-		for task in self.map.iter() {
+		for task in &*self.0 {
 			task.stop();
 		}
 	}
 
 	pub async fn wait(&self) -> Vec<Result<T, Arc<tokio::task::JoinError>>> {
 		let tasks = self
-			.map
+			.0
 			.iter()
 			.map(|entry| entry.value().clone())
 			.collect::<Vec<_>>();
@@ -214,12 +289,28 @@ where
 		self.spawn_with_context(key, || (), |(), stop| f(stop))
 	}
 
+	pub fn spawn_detached<F, Fut>(&self, key: K, f: F) -> Shared<T, ()>
+	where
+		F: FnOnce(Stop) -> Fut,
+		Fut: Future<Output = T> + Send + 'static,
+	{
+		self.spawn_detached_with_context(key, || (), |(), stop| f(stop))
+	}
+
 	pub fn get_or_spawn<F, Fut>(&self, key: K, f: F) -> Shared<T, ()>
 	where
 		F: FnOnce(Stop) -> Fut,
 		Fut: Future<Output = T> + Send + 'static,
 	{
 		self.get_or_spawn_with_context(key, || (), |(), stop| f(stop))
+	}
+
+	pub fn get_or_spawn_detached<F, Fut>(&self, key: K, f: F) -> Shared<T, ()>
+	where
+		F: FnOnce(Stop) -> Fut,
+		Fut: Future<Output = T> + Send + 'static,
+	{
+		self.get_or_spawn_detached_with_context(key, || (), |(), stop| f(stop))
 	}
 
 	pub fn spawn_blocking<F>(&self, key: K, f: F) -> Shared<T, ()>
@@ -229,19 +320,31 @@ where
 		self.spawn_blocking_with_context(key, || (), |(), stop| f(stop))
 	}
 
+	pub fn spawn_blocking_detached<F>(&self, key: K, f: F) -> Shared<T, ()>
+	where
+		F: FnOnce(Stop) -> T + Send + 'static,
+	{
+		self.spawn_blocking_detached_with_context(key, || (), |(), stop| f(stop))
+	}
+
 	pub fn get_or_spawn_blocking<F>(&self, key: K, f: F) -> Shared<T, ()>
 	where
 		F: FnOnce(Stop) -> T + Send + 'static,
 	{
 		self.get_or_spawn_blocking_with_context(key, || (), |(), stop| f(stop))
 	}
+
+	pub fn get_or_spawn_blocking_detached<F>(&self, key: K, f: F) -> Shared<T, ()>
+	where
+		F: FnOnce(Stop) -> T + Send + 'static,
+	{
+		self.get_or_spawn_blocking_detached_with_context(key, || (), |(), stop| f(stop))
+	}
 }
 
 impl<K, T, C, H> Clone for Map<K, T, C, H> {
 	fn clone(&self) -> Self {
-		Self {
-			map: self.map.clone(),
-		}
+		Self(self.0.clone())
 	}
 }
 
@@ -253,7 +356,6 @@ where
 	H: BuildHasher + Default + Clone + Send + Sync + 'static,
 {
 	fn default() -> Self {
-		let map = Arc::new(DashMap::default());
-		Self { map }
+		Self(Arc::new(DashMap::default()))
 	}
 }
