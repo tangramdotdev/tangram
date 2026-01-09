b84a7dfac7e7e2007f07a31734d89a657e3c6f19
David Yamnitsky <david@yamnitsky.com>
2026-01-09 14:07:05 -0500

    refactor(touch): move postgres and sqlite code to separate modules

--- packages/server/src/object/touch.rs
+++ packages/server/src/object/touch.rs
@@ -1,10 +1,13 @@
 use {
 	crate::{Context, Server},
-	indoc::formatdoc,
 	tangram_client::prelude::*,
-	tangram_database::{self as db, prelude::*},
 	tangram_http::{Body, request::Ext as _, response::builder::Ext as _},
 };
+
+#[cfg(feature = "postgres")]
+mod postgres;
+#[cfg(feature = "sqlite")]
+mod sqlite;
 
 impl Server {
 	pub async fn touch_object_with_context(
@@ -43,80 +46,6 @@ impl Server {
 					.map_err(|source| tg::error!(!source, %id, "failed to touch the object"))?;
 			},
 		}
-
-		Ok(())
-	}
-
-	#[cfg(feature = "postgres")]
-	async fn touch_object_postgres(
-		&self,
-		database: &db::postgres::Database,
-		id: &tg::object::Id,
-	) -> tg::Result<()> {
-		let touched_at = time::OffsetDateTime::now_utc().unix_timestamp();
-
-		// Get a connection.
-		let connection = database
-			.connection()
-			.await
-			.map_err(|source| tg::error!(!source, "failed to get a database connection"))?;
-
-		let p = connection.p();
-		let statement = formatdoc!(
-			"
-				update objects
-				set touched_at = greatest(touched_at, {p}1)
-				where id = {p}2;
-			",
-		);
-		let params = db::params![touched_at, id.to_bytes()];
-		let n = connection
-			.execute(statement.into(), params)
-			.await
-			.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
-		if n == 0 {
-			return Err(tg::error!("failed to find the object"));
-		}
-
-		// Drop the connection.
-		drop(connection);
-
-		Ok(())
-	}
-
-	#[cfg(feature = "sqlite")]
-	async fn touch_object_sqlite(
-		&self,
-		database: &db::sqlite::Database,
-		id: &tg::object::Id,
-	) -> tg::Result<()> {
-		let touched_at = time::OffsetDateTime::now_utc().unix_timestamp();
-
-		// Get a connection.
-		let connection = database
-			.write_connection()
-			.await
-			.map_err(|source| tg::error!(!source, "failed to get a database connection"))?;
-
-		let p = connection.p();
-		let statement = formatdoc!(
-			"
-				update objects
-				set touched_at = max(touched_at, {p}1)
-				where id = {p}2;
-			",
-		);
-		let params = db::params![touched_at, id.to_bytes()];
-		let n = connection
-			.execute(statement.into(), params)
-			.await
-			.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
-		if n == 0 {
-			return Err(tg::error!("failed to find the object"));
-		}
-
-		// Drop the connection.
-		drop(connection);
 
 		Ok(())
 	}
--- packages/server/src/process/touch.rs
+++ packages/server/src/process/touch.rs
@@ -1,10 +1,13 @@
 use {
 	crate::{Context, Server},
-	indoc::formatdoc,
 	tangram_client::prelude::*,
-	tangram_database::{self as db, prelude::*},
 	tangram_http::{Body, request::Ext as _, response::builder::Ext as _},
 };
+
+#[cfg(feature = "postgres")]
+mod postgres;
+#[cfg(feature = "sqlite")]
+mod sqlite;
 
 impl Server {
 	pub(crate) async fn touch_process_with_context(
@@ -47,80 +50,6 @@ impl Server {
 					.map_err(|source| tg::error!(!source, %id, "failed to touch the process"))?;
 			},
 		}
-
-		Ok(())
-	}
-
-	#[cfg(feature = "postgres")]
-	async fn touch_process_postgres(
-		&self,
-		database: &db::postgres::Database,
-		id: &tg::process::Id,
-	) -> tg::Result<()> {
-		let touched_at = time::OffsetDateTime::now_utc().unix_timestamp();
-
-		// Get a connection.
-		let connection = database
-			.connection()
-			.await
-			.map_err(|source| tg::error!(!source, "failed to get a database connection"))?;
-
-		let p = connection.p();
-		let statement = formatdoc!(
-			"
-				update processes
-				set touched_at = greatest(touched_at, {p}1)
-				where id = {p}2;
-			",
-		);
-		let params = db::params![touched_at, id.to_bytes()];
-		let n = connection
-			.execute(statement.into(), params)
-			.await
-			.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
-		if n == 0 {
-			return Err(tg::error!("failed to find the process"));
-		}
-
-		// Drop the connection.
-		drop(connection);
-
-		Ok(())
-	}
-
-	#[cfg(feature = "sqlite")]
-	async fn touch_process_sqlite(
-		&self,
-		database: &db::sqlite::Database,
-		id: &tg::process::Id,
-	) -> tg::Result<()> {
-		let touched_at = time::OffsetDateTime::now_utc().unix_timestamp();
-
-		// Get a connection.
-		let connection = database
-			.write_connection()
-			.await
-			.map_err(|source| tg::error!(!source, "failed to get a database connection"))?;
-
-		let p = connection.p();
-		let statement = formatdoc!(
-			"
-				update processes
-				set touched_at = max(touched_at, {p}1)
-				where id = {p}2;
-			",
-		);
-		let params = db::params![touched_at, id.to_bytes()];
-		let n = connection
-			.execute(statement.into(), params)
-			.await
-			.map_err(|source| tg::error!(!source, "failed to execute the statement"))?;
-		if n == 0 {
-			return Err(tg::error!("failed to find the process"));
-		}
-
-		// Drop the connection.
-		drop(connection);
 
 		Ok(())
 	}
