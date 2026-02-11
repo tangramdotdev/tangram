c9957c7690a97a70d3ad9def44119da250d1baca
David Yamnitsky <david@yamnitsky.com>
2026-02-11 13:29:09 -0500

    checkpoint

--- packages/vfs/src/lib.rs
+++ packages/vfs/src/lib.rs
@@ -1,4 +1,4 @@
-use {bytes::Bytes, futures::Future};
+use {bytes::Bytes, futures::Future, std::os::fd::OwnedFd};
 
 pub mod fuse;
 pub mod nfs;
@@ -10,26 +10,64 @@ pub trait Provider {
 	/// Close an open file handle.
 	fn close(&self, handle: u64) -> impl Future<Output = ()> + Send;
 
+	/// Close an open file handle synchronously.
+	fn close_sync(&self, _handle: u64) {}
+
 	/// Get the attributes for a node.
 	fn getattr(&self, id: u64) -> impl Future<Output = Result<Attrs>> + Send;
+
+	/// Get the attributes for a node synchronously.
+	fn getattr_sync(&self, _id: u64) -> Result<Attrs> {
+		Err(std::io::Error::from_raw_os_error(libc::ENOSYS))
+	}
 
 	/// Get the value for an extended attribute for a node.
 	fn getxattr(&self, id: u64, name: &str) -> impl Future<Output = Result<Option<Bytes>>> + Send;
 
+	/// Get the value for an extended attribute for a node synchronously.
+	fn getxattr_sync(&self, _id: u64, _name: &str) -> Result<Option<Bytes>> {
+		Err(std::io::Error::from_raw_os_error(libc::ENOSYS))
+	}
+
 	/// List extended attributes for a node.
 	fn listxattrs(&self, id: u64) -> impl Future<Output = Result<Vec<String>>> + Send;
+
+	/// List extended attributes for a node synchronously.
+	fn listxattrs_sync(&self, _id: u64) -> Result<Vec<String>> {
+		Err(std::io::Error::from_raw_os_error(libc::ENOSYS))
+	}
 
 	/// Look up a node.
 	fn lookup(&self, id: u64, name: &str) -> impl Future<Output = Result<Option<u64>>> + Send;
 
+	/// Look up a node synchronously.
+	fn lookup_sync(&self, _id: u64, _name: &str) -> Result<Option<u64>> {
+		Err(std::io::Error::from_raw_os_error(libc::ENOSYS))
+	}
+
 	/// Look up a node's parent.
 	fn lookup_parent(&self, id: u64) -> impl Future<Output = Result<u64>> + Send;
+
+	/// Look up a node's parent synchronously.
+	fn lookup_parent_sync(&self, _id: u64) -> Result<u64> {
+		Err(std::io::Error::from_raw_os_error(libc::ENOSYS))
+	}
 
 	/// Open a file.
 	fn open(&self, id: u64) -> impl Future<Output = Result<u64>> + Send;
 
+	/// Open a file synchronously.
+	fn open_sync(&self, _id: u64) -> Result<(u64, Option<OwnedFd>)> {
+		Err(std::io::Error::from_raw_os_error(libc::ENOSYS))
+	}
+
 	/// Open a directory.
 	fn opendir(&self, id: u64) -> impl Future<Output = Result<u64>> + Send;
+
+	/// Open a directory synchronously.
+	fn opendir_sync(&self, _id: u64) -> Result<u64> {
+		Err(std::io::Error::from_raw_os_error(libc::ENOSYS))
+	}
 
 	/// Read from a file.
 	fn read(
@@ -39,11 +77,26 @@ pub trait Provider {
 		length: u64,
 	) -> impl Future<Output = Result<Bytes>> + Send;
 
+	/// Read from a file synchronously.
+	fn read_sync(&self, _handle: u64, _position: u64, _length: u64) -> Result<Bytes> {
+		Err(std::io::Error::from_raw_os_error(libc::ENOSYS))
+	}
+
 	/// Read from a directory.
 	fn readdir(&self, handle: u64) -> impl Future<Output = Result<Vec<(String, u64)>>> + Send;
 
+	/// Read from a directory synchronously.
+	fn readdir_sync(&self, _handle: u64) -> Result<Vec<(String, u64)>> {
+		Err(std::io::Error::from_raw_os_error(libc::ENOSYS))
+	}
+
 	/// Read from a symlink.
 	fn readlink(&self, id: u64) -> impl Future<Output = Result<Bytes>> + Send;
+
+	/// Read from a symlink synchronously.
+	fn readlink_sync(&self, _id: u64) -> Result<Bytes> {
+		Err(std::io::Error::from_raw_os_error(libc::ENOSYS))
+	}
 }
 
 #[derive(Clone, Copy, Debug)]
