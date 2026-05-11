1fb6e5cf4b9c2ec017d47abf34855f8fb2145e72
Ben Lovy <ben@deciduously.com>
2026-05-11 11:35:57 -0400

    fix(sandbox/macos): canonicalize current_exe path

--- packages/util/src/env.rs
+++ packages/util/src/env.rs
@@ -11,7 +11,7 @@ pub fn current_exe() -> std::io::Result<PathBuf> {
 		let path = OsStr::from_bytes(path);
 		PathBuf::from(path)
 	} else {
-		path
+		std::fs::canonicalize(&path)?
 	};
 	Ok(path)
 }
